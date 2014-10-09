b47eaf0ff49b54fc3a9f1883092c2dc28f54b830
Eric Rasche <rasche.eric@yandex.ru>
2014-10-09 17:55:21 -0500

    MIST mostly complete

--- bin/mist.py
+++ bin/mist.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 from galaxygetopt.ggo import GalaxyGetOpt as GGO
+from galaxygetopt.outputfiles import OutputFiles
 import tempfile
+import shutil
 import logging
 import os
+import math
 logging.basicConfig(level=logging.INFO)
 from Bio import SeqIO
 import subprocess
@@ -13,6 +16,84 @@ Multiple Interrlated Sequence doTplotter
 ========================================
 
 """
+
+
+def add_image_border(outfile, i, j, file_data, img_data, zoom):
+    half_height = img_data['orig_height']/2
+    half_width = img_data['orig_width']/2
+
+    cmd = ['convert', img_data['orig'],
+           '-bordercolor', 'purple',
+           '-border', '2x2',
+           '-bordercolor', 'gray',
+           '-border', '50x50',
+           '-rotate', '-90',
+           '-pointsize', '40',
+           '-font', 'Ubuntu-Mono-Regular',
+           '-fill', 'black', '-annotate',
+           '+%s+%s' % (half_height, img_data['orig_width'] + 50 + 45), file_data[i]['header'],
+           '-pointsize', '20',
+           ]
+    for z in range(0, int(.8 * file_data[j]['seqlen']/zoom), int(BestTick(file_data[j]['seqlen'], 5)/zoom)):
+        label = label_formatter(z, zoom)
+        cmd += [
+            '-fill', 'grey22', '-stroke', 'none', '-annotate', '+%s+%s' % (z + 56, 50 + img_data['orig_width'] + 20), label
+        ]
+
+    cmd += ['-rotate', '90',
+            '-pointsize', '40',
+            '-fill', 'black', '-annotate',
+            '+%s+30' % half_width, file_data[i]['header'],
+            '-pointsize', '20',
+            '-annotate', '+%s+%s' % (2, 50 + img_data['orig_height'] + 30),
+            "Produced by the CPT's MIST (Multiple Interrelated Sequence doT plotter). Written by Eric Rasche <esr@tamu.edu>.\nDot plots produced by the Gepard Dot Plotter by Dr. Jan Krumsiek",
+            ]
+    for z in range(0, int(.8 * file_data[i]['seqlen']/zoom), int(BestTick(file_data[i]['seqlen'], 5)/zoom)):
+        label = label_formatter(z, zoom)
+        cmd += [
+            '-fill', 'grey22', '-stroke', 'none', '-annotate', '+%s+50' % (z + 56), label
+        ]
+
+    for z in range(0, int(file_data[i]['seqlen']/zoom), int(BestTick(file_data[i]['seqlen'], 5)/zoom)):
+        label = label_formatter(z, zoom)
+        cmd += [
+            '-fill', 'white', '-stroke', 'grey22', '-strokewidth', '2', '-draw', 'line %s,35 %s,50' % (z + 51, z + 51),
+        ]
+
+    for z in range(0, int(file_data[j]['seqlen']/zoom), int(BestTick(file_data[j]['seqlen'], 5)/zoom)):
+        cmd += [
+            '-fill', 'white', '-stroke', 'grey22', '-strokewidth', '2', '-draw', 'line 35,%s 50,%s' % (z + 51, z + 51),
+        ]
+
+    cmd.append(outfile)
+    subprocess.check_output(cmd)
+
+
+def label_formatter(bases, zoom):
+    label = bases * zoom
+    if label > 1000000:
+        label = "%s Mbp" % int(label/1000000)
+    elif label > 1000:
+        label = "%s kbp" % int(label/1000)
+    else:
+        label = "%s b" % label
+    return label
+
+
+def BestTick(largest, mostticks):
+    # http://stackoverflow.com/a/361687
+    minimum = largest / mostticks
+    magnitude = 10 ** math.floor(math.log(minimum) / math.log(10))
+    residual = minimum / magnitude
+    if residual > 5:
+        tick = 10 * magnitude
+    elif residual > 2:
+        tick = 5 * magnitude
+    elif residual > 1:
+        tick = 2 * magnitude
+    else:
+        tick = magnitude
+    return tick
 
 
 def extract_info_from_file(infile, label, tmpdir):
@@ -37,14 +118,15 @@ def extract_info_from_file(infile, label, tmpdir):
 
 
 def run_gepard(seq_a, seq_b, zoom, output):
+    print "Running %s vs %s" % (seq_a, seq_b)
     cmd = ['java', '-jar', 'gepard.jar',
            '--seq1', seq_a,
            '--seq2', seq_b,
            '--matrix', 'edna.mat',
            '--outfile', output,
-           '--zoom', zoom
+           '--zoom', zoom,
+           '--silent'
            ]
-    print ' '.join(cmd)
     subprocess.check_call(cmd)
 
 
@@ -53,7 +135,7 @@ def resize_image(scale, from_file, to_file):
     subprocess.check_call(cmd)
 
 
-def mist(file, label, zoom, java_7_bin, *args, **kwargs):
+def mist(ggo, file, label, zoom, *args, **kwargs):
     inputs = zip(file, label)
 
     tmpdir = tempfile.mkdtemp(prefix="cpt.mist.")
@@ -68,7 +150,7 @@ def mist(file, label, zoom, java_7_bin, *args, **kwargs):
             ordering.append(f['id'])
             file_data[f['id']] = f
 
-    for subdir in ['png', 'thumb', 'prefinal', 'final']:
+    for subdir in ['png', 'thumb', 'prefinal']:
         directory = os.path.join(tmpdir, subdir)
         if not os.path.exists(directory):
             os.makedirs(directory)
@@ -92,8 +174,6 @@ def mist(file, label, zoom, java_7_bin, *args, **kwargs):
                        zoom, gepard_png)
             resize_image(rescale_p, gepard_png, thumb_png)
             img_array[i][j] = {
-                'from': i,
-                'to': j,
                 'orig': gepard_png,
                 'thumb': thumb_png,
             }
@@ -132,49 +212,40 @@ def mist(file, label, zoom, java_7_bin, *args, **kwargs):
         for j in ordering:
             current_image = img_array[i][j]
             output = subprocess.check_output(['identify',
-                                              current_image['thumb']])
+                                              current_image['orig']])
             size = output.split(' ')[3]
             (w, h) = size[0:size.index('+')].split('x')
-            img_array[i][j]['width'] = int(w)
-            img_array[i][j]['height'] = int(h)
+            img_array[i][j]['width'] = int(w) * rescale
+            img_array[i][j]['height'] = int(h) * rescale
+            img_array[i][j]['orig_width'] = int(w)
+            img_array[i][j]['orig_height'] = int(h)
 
             if ordering.index(i) == 0:
-                cumulative_width += int(w)
+                cumulative_width += img_array[i][j]['width'] + 2
 
             if ordering.index(j) == 0:
-                cumulative_height += int(h)
+                cumulative_height += img_array[i][j]['height'] + 2
 
-    #import pprint; pprint.pprint(img_array)
-    border_size = 2 * inter_image_borders * cardinality
+            gepard_annot_png = os.path.join(tmpdir, 'png', 'a_%s-%s.png' % (i, j))
+            add_image_border(gepard_annot_png, i, j, file_data,
+                             img_array[i][j], int(zoom))
+            img_array[i][j]['annotated_original'] = gepard_annot_png
+
     # The +1 and +2 are as a result of adding a 1 width purple border, so the border is consistent everywhere.
-    total_size_one_border = cumulative_width + border_size
-    current_sum = 1 + inter_image_borders
+    current_sum = 51 + inter_image_borders
     convert_arguments_top = []
     convert_arguments_left = []
-    left_offset = total_size_one_border + 20
-
-    # Original, bad
-    # -pointsize 24 -font Ubuntu-Mono-Regular -fill black -annotate +3+1385 ANI
-    # -fill black -annotate +458+1385 Karma -fill black -annotate +913+1385 fasta
-    # -rotate 90 -fill black -annotate +3+30 ANI -fill black -annotate +458+30 Karma
-    # -fill black -annotate +913+30 fasta -pointsize 14 -annotate +2+1385 "test"
-
-    # Manual, good
-    # -pointsize 20 -font Ubuntu-Mono-Regular -fill black -annotate +53+1085 ANI
-    # -fill black -annotate +508+1085 Karma -fill black -annotate +963+1085 fasta
-    # -rotate 90 -fill black -annotate +53+40 ANI -fill black -annotate +508+40 Karma
-    # -fill black -annotate +963+40 fasta -pointsize 14 -annotate +2+1100 "test"
+    left_offset = cumulative_width + 51 + 20
 
     for i in ordering:
         current_image = img_array[i][i]
-        print current_image
         convert_arguments_top += [
             '-fill', 'black', '-annotate',
-            '+%s+30' % current_sum, file_data[i]['header']
+            '+%s+40' % current_sum, file_data[i]['header']
         ]
         convert_arguments_left += [
             '-fill', 'black', '-annotate',
-            '+%s+%s' % (current_sum, left_offset), file_data[i]['header']
+            '+%s+%s' % (current_sum - 10, left_offset - 40), file_data[i]['header']
         ]
 
         current_sum += current_image['width'] + (2 * inter_image_borders)
@@ -182,7 +253,7 @@ def mist(file, label, zoom, java_7_bin, *args, **kwargs):
     m2 = os.path.join(tmpdir, 'prefinal', 'montage_2.png')
     cmd = ['convert', m1,
            '-rotate', '-90',
-           '-pointsize', '24',
+           '-pointsize', '20',
            '-font', 'Ubuntu-Mono-Regular',
            ]
     cmd += convert_arguments_left
@@ -192,10 +263,63 @@ def mist(file, label, zoom, java_7_bin, *args, **kwargs):
         '-pointsize', '14',
         '-annotate', '+%s+%s' % (2, left_offset),
         "Produced by the CPT's MIST (Multiple Interrelated Sequence doT plotter). Written by Eric Rasche <rasche.eric\@yandex.ru>.\nDot plots produced by the Gepard Dot Plotter by Dr. Jan Krumsiek",
-        os.path.join(tmpdir, 'final', 'large.png')
+        os.path.join(tmpdir, 'prefinal', 'large.png')
     ]
     print ' '.join(cmd)
     subprocess.check_output(cmd)
+
+    imagemap = ""
+
+    files_to_move = [
+        (os.path.join(tmpdir, 'prefinal', 'large.png'), 'large')
+    ]
+
+    cur_y = 51
+    for j in ordering:
+        cur_x = 51
+        tmp_height = 0
+        for i in ordering:
+            current = img_array[i][j]
+            width = current['width']
+            height = current['height']
+            tmp_height = height
+            files_to_move.append((img_array[i][j]['annotated_original'],
+                                  'a_%s-%s' % (i, j)))
+            print current
+            imagemap += '<area shape="rect" coords="%s,%s,%s,%s" alt="%s" href="%s" />\n' \
+                % (int(cur_x),
+                   int(cur_y),
+                   int(cur_x+width+2),
+                   int(cur_y+height+2),
+                    "%s vs %s" % (file_data[i]['header'], file_data[j]['header']),
+                   'a_%s-%s.png' % (i, j)
+                   )
+            cur_x += width + 4
+        cur_y += tmp_height + 4
+
+
+    html_page = """
+    <!DOCTYPE html>
+    <html>
+    <body>
+    <h1>Mist Results</h1>
+    <p>Each section of mist output is now clickable to view a higher resolution version of that subsection</p>
+    <img src="large.png" usemap="#mistmap">
+    <map name="mistmap">
+    %s
+    </map>
+    </body>
+    </html>
+    """ % imagemap
+
+    of = OutputFiles(name='dotplot', GGO=ggo)
+    of.CRR(data=html_page)
+
+    for (original, new) in files_to_move:
+        produced_files = of.subCRR(data_format="dummy", format_as="Dummy",
+                                   filename=new, extension='png', data="dummy")
+        destination = produced_files[0]
+        shutil.copy(original, destination)
 
 
 if __name__ == '__main__':
@@ -208,8 +332,6 @@ if __name__ == '__main__':
              {'required': True, 'multiple': True, 'validate': 'String'}],
             ['zoom', 'How zoomed in the image is. Be careful with this option. It represents the number of bases to plot in a single pixel. For large genomes, this can mean very large images, and should be lowered appropriately. For a value of 50, 50 bases would be considered a single pixel in the output image. For 1Mbp of genomes totaly (say 5 x 200 kb phages), this would result in a 20,000 pixel image.',
              {'required': True, 'validate': 'String', 'default': 50}],
-            ['java_7_bin', 'Location of the Java binary',
-             {'required': True, 'default': '/usr/lib/jvm/java-7-openjdk-amd64/bin/java', 'hidden': True, 'validate': 'String'}],
         ],
         outputs=[
             [
@@ -234,9 +356,6 @@ if __name__ == '__main__':
         doc=__doc__
     )
     options = opts.params()
-    result = mist(**options)
+    result = mist(ggo=opts, **options)
     #svg_xml = generate_viz(blast_file=options['blast'])
 
-    #from galaxygetopt.outputfiles import OutputFiles
-    #of = OutputFiles(name='plot', GGO=opts)
-    #of.CRR(data=svg_xml)
