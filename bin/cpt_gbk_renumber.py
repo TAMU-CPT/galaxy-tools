81180e7ae3184da608b0765c1c70ee2cba30dd49
Eric Rasche <rasche.eric@yandex.ru>
2015-01-22 15:08:51 -0600

    Fix renumbering to not include tRNAs

--- bin/cpt_gbk_renumber.py
+++ bin/cpt_gbk_renumber.py
@@ -34,6 +34,7 @@ def renumber_genes(gbk_file=None, tag_to_update="locus_tag",
         f_gene = [f for f in records[i].features if f.type == 'gene']
         f_oth = [f for f in records[i].features if f.type not in ['CDS', 'RBS',
                                                                   'gene']]
+        f_care_about = []
 
         # Make sure we've hit every RBS and gene
         h_rbs = {}
@@ -61,42 +62,31 @@ def renumber_genes(gbk_file=None, tag_to_update="locus_tag",
             # We choose to append to f_other as that has all features not
             # already accessed. It may mean that some gene/RBS features are
             # missed if they aren't detected here, which we'll need to handle.
-            f_oth.append(tmp_result)
-
-        def delta_old(feature):
-            # First part of delta entry, old name
-            val = '%s %s %s' % (f.location.start, f.location.end,
-                                f.location.strand,)
-            if tag_to_update in f.qualifiers:
-                val = f.qualifiers[tag_to_update][0]
-            return val
+            f_care_about.append(tmp_result)
 
         # Why must these people start at 1
         index = 1
         # Because we have to modify the array we work on a separate one
-        clean_features = []
-        for feature in f_oth:
-            # Lists are groups of cds+gene?+rbs? that need to be renumbered.
-            if isinstance(feature, list):
-                 #Renumber all features in list
-                for f in feature:
-                    delta_entry = [delta_old(f)]
-                    f.qualifiers[tag_to_update] = [format_string % index]
-                    clean_features.append(f)
-                    delta_entry.append(format_string % index)
-                    delta['Sheet1']['data'].append(delta_entry)
-            else:
-                delta_entry = [delta_old(f)]
-                print f
-                feature.qualifiers[tag_to_update] = [format_string % index]
-                clean_features.append(feature)
-                delta_entry.append(format_string % index)
-                delta['Sheet1']['data'].append(delta_entry)
+        clean_features = f_oth
+        for feature_list in sorted(f_care_about, key=lambda x: x[0].location.start):
+            for f in feature_list:
+                f.qualifiers[tag_to_update] = [format_string % index]
+                clean_features.append(f)
+                delta['Sheet1']['data'].append([
+                    delta_old(f, tag_to_update), format_string % index ])
             index += 1
 
         # Update all features
-        records[i].features = clean_features
+        records[i].features = sorted(clean_features, key=lambda x: x.location.start)
     return (records, delta)
+
+def delta_old(feature, tag_to_update):
+    # First part of delta entry, old name
+    val = '%s %s %s' % (feature.location.start, feature.location.end,
+                        feature.location.strand,)
+    if tag_to_update in feature.qualifiers:
+        val = feature.qualifiers[tag_to_update][0]
+    return val
 
 
 if __name__ == '__main__':
