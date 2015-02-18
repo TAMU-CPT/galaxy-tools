12af35a723d45267ff9202d368fa5aa7fefab879
Eric Rasche <rasche.eric@yandex.ru>
2015-02-18 14:17:18 -0600

    Added translate tools

---
+++ bin/cpt_translate.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import logging
+import random
+logging.basicConfig(level=logging.INFO)
+import argparse
+from Bio import SeqIO, Seq
+import StringIO
+
+def translate(fasta_file, table=11, strip_stops=False):
+    records = list(SeqIO.parse(fasta_file, "fasta"))
+    output = StringIO.StringIO()
+
+    # If we strip stops, then OK as is, otherwise add the extra char that the
+    # [0:] would've missed
+    strip_stops_diff = 0 if strip_stops else -1
+
+    for record in records:
+        tmpseq = record.seq.translate(table=table)
+        if '*' in tmpseq:
+            tmpseq = tmpseq[0:str(tmpseq).index('*') - strip_stops_diff]
+        record.seq = tmpseq
+        if len(record.seq) > 0:
+            SeqIO.write(record, output, "fasta")
+
+    print output.getvalue()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Translate fasta file')
+    parser.add_argument('fasta_file', type=file, help='Fasta file')
+    parser.add_argument('--table', type=int, default=11, help='Translation table to use', choices=range(1,23))
+    parser.add_argument('--strip_stops', action='store_true', help='Remove stop characters')
+
+    args = parser.parse_args()
+    translate(**vars(args))
