1c6f8e7aa610630f7ac1dc767d69aac9e5a109e0
Eric Rasche <rasche.eric@yandex.ru>
2015-04-29 16:25:08 -0500

    BC fasta

---
+++ bin/cpt_blastclust_fasta.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from Bio import SeqIO
+import StringIO
+import argparse
+import logging
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Identify shine-dalgarno sequences')
+    parser.add_argument('fasta_file', type=file, help='Blastclust fasta input')
+    parser.add_argument('blastclust', help='Blastclust Output')
+    parser.add_argument('--ignore', type=int, help="Clusters smaller than this threshold will be ignored")
+    args = parser.parse_args()
+
+    id_to_clust = {}
+    with open(args.blastclust, 'r') as handle:
+        for i, line in enumerate(handle):
+            for clust_id in line.strip().split():
+                id_to_clust[clust_id] = i
+
+    for sequence in SeqIO.parse(args.fasta_file, 'fasta'):
+        if sequence.id in id_to_clust:
+            name = 'blastclust_%s.fa' % id_to_clust[sequence.id]
+            with open(name, 'a') as handle:
+                tmp = StringIO.StringIO("HI")
+                SeqIO.write([sequence], tmp, 'fasta')
+                handle.write(tmp.getvalue())
