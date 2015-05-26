6cde421bce7c0581b548a5d447f799eaf146374d
Eric Rasche <esr@tamu.edu>
2015-05-26 18:27:50 -0500

    Deprecate due to https://github.com/galaxyproject/tools-iuc/pull/106

--- tools/gbk/source.py
+++ tools/gbk/source.py
@@ -1,15 +1,75 @@
 #!/usr/bin/env python
 import argparse
 from Bio import SeqIO
-from cpt_ncbi_efetch_gbk import name_parser
+import re
+import logging
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger()
+
+phage_in_middle = re.compile('^(?P<host>.*)\s*phage (?P<phage>.*)$')
+bacteriophage_in_middle = re.compile('^(?P<host>.*)\s*bacteriophage (?P<phage>.*)$')
+starts_with_phage = re.compile('^(bacterio|vibrio|Bacterio|Vibrio|)?[Pp]hage (?P<phage>.*)$')
+new_style_names = re.compile('(?P<phage>v[A-Z]_[A-Z][a-z]{2}_.*)')
+
+
+def name_parser(name):
+    host = None
+    phage = None
+    name = name.replace(', complete genome', '')
+
+    m = bacteriophage_in_middle.match(name)
+    if m:
+        host = m.group('host')
+        phage = m.group('phage')
+        return (host, phage)
+
+    m = phage_in_middle.match(name)
+    if m:
+        host = m.group('host')
+        phage = m.group('phage')
+        return (host, phage)
+
+    m = starts_with_phage.match(name)
+    if m:
+        phage = m.group('phage')
+        return (host, phage)
+
+    m = new_style_names.match(name)
+    if m:
+        phage = m.group('phage')
+        return (host, phage)
+
+    return (host, phage)
+
+
+def extract_host_info(host_string):
+    source_host = host_string.split(' ')
+    if len(source_host) > 3:
+        other = ' '.join(source_host[3:])
+    else:
+        other = ""
+
+    if len(source_host) > 2:
+        strain = source_host[2]
+    else:
+        strain = ""
+
+    if len(source_host) > 1:
+        species = source_host[1]
+    else:
+        species = ""
+
+    if len(source_host) > 0:
+        genus = source_host[0]
+    else:
+        genus = ""
+    return (id, genus, species, strain, other)
 
 
 def phage_source(genbank_files=None, **kwargs):
     for genbank_file in genbank_files:
         for record in SeqIO.parse(genbank_file, "genbank"):
             id = record.id
-            if '.' in id:
-                id = id.split('.')[0]
 
             source_feats = [x for x in record.features if x.type == 'source']
 
@@ -23,31 +83,17 @@ def phage_source(genbank_files=None, **kwargs):
             # If there isn't a source feature, return that immediately
             if len(source_feats) == 0:
                 yield ret
-            else:
+            elif len(source_feats) == 1:
                 # Otherwise look through any and all source features for a /host qualifier
-                for source_feat in source_feats:
-                    if 'host' in source_feat.qualifiers:
-                        source_host = source_feat.qualifiers['host'][0].split(' ')
-                        if len(source_host) > 3:
-                            other = ' '.join(source_host[3:])
-                        else:
-                            other = ""
-
-                        if len(source_host) > 2:
-                            strain = source_host[2]
-                        else:
-                            strain = ""
-
-                        if len(source_host) > 1:
-                            species = source_host[1]
-                        else:
-                            species  = ""
-
-                        if len(source_host) > 0:
-                            genus = source_host[0]
-                        else:
-                            genus = ""
-                        ret = (id, genus, species, strain, other)
+                if 'host' in source_feats[0].qualifiers:
+                    ret = extract_host_info(source_feats[0].qualifiers['host'][0])
+                yield ret
+            else:
+                log.warning("Record with multiple source features. Unlikely to "
+                            "be well behaved. Please email the developer with "
+                            "this record %s", record.id)
+                if 'host' in source_feats[0].qualifiers:
+                    ret = extract_host_info(source_feats[0].qualifiers['host'][0])
                 yield ret
 
 if __name__ == '__main__':
