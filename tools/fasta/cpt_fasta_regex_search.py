5dc3b713d7f28165f55ad26883381275ce5a8289
Eric Rasche <esr@tamu.edu>
2015-05-26 15:47:06 -0500

    Update sanitizer

--- tools/fasta/cpt_fasta_regex_search.py
+++ tools/fasta/cpt_fasta_regex_search.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python
+import sys
 import re
-import logging
-logging.basicConfig(level=logging.INFO)
 import argparse
 from Bio import SeqIO
-import StringIO
+from Bio.Seq import Seq
+import logging
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger()
+
 
 def search(fasta, regex):
     m = re.compile(regex)
-    good_records = []
     for record in SeqIO.parse(fasta, "fasta"):
         if m.search(str(record.seq).upper()):
-            good_records.append(record)
-    return good_records
+            oseq = str(record.seq).upper()
+            newseq = ""
+            last_idx = 0
+            for hit in m.finditer(oseq):
+                newseq += oseq[last_idx:hit.start()].lower()
+                newseq += oseq[hit.start():hit.end()].upper()
+                last_idx = hit.end()
+            newseq += oseq[last_idx:].lower()
+            # Update sequence
+            record.seq = Seq(newseq)
+            yield [record]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Filter sequences based on regular expressions')
     parser.add_argument('fasta', type=file, help='Fasta Sequence')
-    parser.add_argument('regex', help='Regular Expression')
+    parser.add_argument('regex', help='Regular Expression (use upper case sequences)')
 
     args = parser.parse_args()
 
-    output = StringIO.StringIO()
-    records = search(**vars(args))
-    SeqIO.write(records, output, "fasta")
-    print output.getvalue()
+    for record in search(**vars(args)):
+        SeqIO.write(record, sys.stdout, "fasta")
