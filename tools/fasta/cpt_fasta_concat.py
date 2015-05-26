0085b359358286182f319e6d061b9192d258946d
Eric Rasche <esr@tamu.edu>
2015-05-26 15:14:24 -0500

    fasta concat updated

--- tools/fasta/cpt_fasta_concat.py
+++ tools/fasta/cpt_fasta_concat.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
-from galaxygetopt.ggo import GalaxyGetOpt as GGO
+import sys
+import argparse
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+
 import logging
 logging.basicConfig(level=logging.INFO)
+log = logging.getLogger()
 
 
-__doc__ = """
-GenBank Feature Export
-======================
-
-Exports features from a GenBank file
-"""
-
-
-def extract_features(fasta_file=None, id="merged"):
-    from Bio import SeqIO
-    from Bio.SeqRecord import SeqRecord
+def merge(fasta_file=None, id="merged"):
     sequence = ''
     ids = []
     records = list(SeqIO.parse(fasta_file, "fasta"))
@@ -28,37 +23,9 @@ def extract_features(fasta_file=None, id="merged"):
 
 
 if __name__ == '__main__':
-    # Grab all of the filters from our plugin loader
-    opts = GGO(
-        options=[
-            ['file', 'Fasta file to merge', {'required': True, 'validate':
-                                             'File/Input'}],
-            ['id', 'New fasta identifier for merged sequences', {'required': True, 'validate': 'String', 'default': 'merged'}],
-        ],
-        outputs=[
-            [
-                'fasta',
-                'Concatenated fasta file',
-                {
-                    'validate': 'File/Output',
-                    'required': True,
-                    'default': 'export',
-                    'data_format': 'genomic/raw',
-                    'default_format': 'Fasta',
-                }
-            ]
-        ],
-        defaults={
-            'appid': 'edu.tamu.cpt.genbank.fasta-merge',
-            'appname': 'Concatenate Fasta Sequences',
-            'appvers': '1.94',
-            'appdesc': 'merge into single long sequence',
-        },
-        tests=[],
-        doc=__doc__
-    )
-    options = opts.params()
-    result = extract_features(fasta_file=options['file'], id=options['id'])
-    from galaxygetopt.outputfiles import OutputFiles
-    of = OutputFiles(name='fasta', GGO=opts)
-    of.CRR(data=result)
+    parser = argparse.ArgumentParser(description='Identify shine-dalgarno sequences')
+    parser.add_argument('fasta_file', type=file, help='Genbank file')
+    parser.add_argument('--id', help='New fasta identifier for merged sequences', default='merged')
+
+    args = parser.parse_args()
+    SeqIO.write(merge(**vars(args)), sys.stdout, "fasta")
