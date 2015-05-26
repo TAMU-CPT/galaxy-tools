dad1201289e5d80c64f329dc397ef47883fee002
Eric Rasche <esr@tamu.edu>
2015-05-26 15:53:41 -0500

    Reopend cleaned up

---
+++ tools/fasta/reopen.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import argparse
+from Bio import SeqIO
+
+import logging
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger()
+
+
+def reopen_contig(fasta_file, after):
+    for record in SeqIO.parse(fasta_file, "fasta"):
+        seq = record.seq
+        record.seq = seq[after:] + seq[0:after]
+        yield [record]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Reopen contig')
+    parser.add_argument('fasta_file', type=file, help='Fasta file')
+    parser.add_argument('after', type=int, help='Reopen contig after this base (1-indexed)')
+
+    args = parser.parse_args()
+    for seq in reopen_contig(**vars(args)):
+        print SeqIO.write(seq, sys.stdout, 'fasta')
