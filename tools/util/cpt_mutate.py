941544c85baeb5a4820a3f1a29dd082e6dd06515
Eric Rasche <esr@tamu.edu>
2015-05-26 19:06:58 -0500

    Mutation util updated

--- tools/util/cpt_mutate.py
+++ tools/util/cpt_mutate.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-import logging
-import random
-import copy
-logging.basicConfig(level=logging.INFO)
 import argparse
-from Bio import SeqIO, Seq
-import StringIO
+import sys
+import copy
+from Bio import SeqIO
+from Bio.Seq import Seq
+import logging
+logging.basicConfig(level=logging.INFO)
 
 
 def mutate(char):
@@ -13,19 +13,20 @@ def mutate(char):
     del targets[targets.index(char)]
     return targets
 
+
 def insert(char):
     return [char + x for x in ('A', 'C', 'T', 'G')]
+
 
 def delete(char):
     return [""]
 
+
 def snp(fasta_file, mutation='mutate'):
-    records = list(SeqIO.parse(fasta_file, "fasta"))
-    output = StringIO.StringIO()
 
     methodToCall = globals()[mutation]
 
-    for record in records:
+    for record in SeqIO.parse(fasta_file, "fasta"):
         seq = str(record.seq).upper()
         # For every character
         for i in [-1] + range(len(seq)):
@@ -44,23 +45,24 @@ def snp(fasta_file, mutation='mutate'):
                 # Get all possible mutations
                 for mutated in methodToCall(seq[i]):
                     # And add those
-                    results.append(seq[0:i] + mutated + seq[i+1:])
+                    results.append(seq[0:i] + mutated + seq[i + 1:])
                     messages.append('[%s at %s: %s]' % (mutation, i + 1, mutated))
                     ids.append('_%s%s%s' % (mutation[0], i + 1, mutated))
 
             if results is not None:
                 for (result, message, id_add) in zip(results, messages, ids):
                     rec_copy = copy.deepcopy(record)
-                    rec_copy.seq = Seq.Seq(result)
+                    rec_copy.seq = Seq(result)
                     rec_copy.description += message
                     rec_copy.id += id_add
-                    SeqIO.write(rec_copy, output, "fasta")
-    print output.getvalue()
+                    yield [rec_copy]
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='make SNPs in fasta files')
+    parser = argparse.ArgumentParser(description='Generate ALL possible SNPs in fasta sequences')
     parser.add_argument('fasta_file', type=file, help='Fasta file')
-    parser.add_argument('mutation', type=str, help='Type of mutation to make', default='mutate', choices=['mutate', 'insert', 'delete'])
+    parser.add_argument('mutation', type=str, help='Type of mutation to make',
+                        default='mutate', choices=['mutate', 'insert', 'delete'])
 
     args = parser.parse_args()
-    snp(**vars(args))
+    for record in snp(**vars(args)):
+        SeqIO.write(record, sys.stdout, "fasta")
