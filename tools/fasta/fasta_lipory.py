6aa81bfcf7e0bef55d3bf810f766e5364b901418
Eric Rasche <esr@tamu.edu>
2015-05-29 21:00:45 -0500

    Fix lipory for new requests

--- tools/fasta/fasta_lipory.py
+++ tools/fasta/fasta_lipory.py
@@ -3,8 +3,7 @@ import re
 import sys
 import argparse
 from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
+from BCBio import GFF
 
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -13,55 +12,32 @@ log = logging.getLogger()
 LIPOBOX = re.compile('[ILMFTV][^REKD][GAS]C')
 
 
-def get_id(feature=None, parent_prefix=None, idx=None):
-    result = ""
-    if parent_prefix is not None:
-        result += parent_prefix + '|'
-    if idx is not None:
-        result += '%03d_' % idx
-    if 'locus_tag' in feature.qualifiers:
-        result += feature.qualifiers['locus_tag'][0]
-    elif 'gene' in feature.qualifiers:
-        result += feature.qualifiers['gene'][0]
-    elif 'product' in feature.qualifiers:
-        result += feature.qualifiers['product'][0]
-    else:
-        result += '%s_%s_%s' % (feature.location.start, feature.location.end,
-                                feature.location.strand)
-    return result
+def find_lipoprotein(gff3_file, fasta_genome, lipobox_mindist=10, lipobox_maxdist=30):
+    seq_dict = SeqIO.to_dict(SeqIO.parse(fasta_genome, "fasta"))
 
+    for record in GFF.parse(gff3_file, base_dict=seq_dict):
+        good_features = []
+        for feature in record.features:
+            if feature.type == 'remark':
+                continue
 
-def find_lipoprotein(fasta_file, lipobox_mindist=10, lipobox_maxdist=30):
-    for record in SeqIO.parse(fasta_file, "fasta"):
-        if LIPOBOX.search(str(record.seq)):
-
-            newseq = ""
-            last_idx = 0
-            should_yield = False
-            for hit in LIPOBOX.finditer(str(record.seq)):
-                if lipobox_mindist < hit.start() < lipobox_maxdist:
-                    newseq += str(record.seq)[last_idx:hit.start()].lower()
-                    newseq += str(record.seq)[hit.start():hit.end()].upper()
-                    last_idx = hit.end()
-                    should_yield = True
-
-            newseq += str(record.seq)[last_idx:].lower()
-            # Update sequence
-
-            if should_yield:
-                yield [SeqRecord(Seq(newseq), id=record.id,
-                                    description=record.description)]
+            tmpseq = str(feature.extract(record.seq).translate(table=11)).replace("*", "")
+            if LIPOBOX.search(tmpseq):
+                good_features.append(feature)
+        record.features = good_features
+        yield [record]
 
 
 if __name__ == '__main__':
-    # Grab all of the filters from our plugin loader
     parser = argparse.ArgumentParser(description='Filter out lipoproteins', epilog="")
-    parser.add_argument('fasta_file', type=file, help='Genbank file')
+    parser.add_argument('gff3_file', type=file, help='Naive ORF Calls')
+    parser.add_argument('fasta_genome', type=file, help='Fasta genome sequence')
+
     parser.add_argument('--lipobox_mindist', type=int, help='Minimum distance in codons to start of lipobox', default=10)
     parser.add_argument('--lipobox_maxdist', type=int, help='Maximum distance in codons to start of lipobox', default=33)
 
     args = parser.parse_args()
 
     args = vars(parser.parse_args())
-    for seq in find_lipoprotein(**args):
-        SeqIO.write(seq, sys.stdout, 'fasta')
+    for record in find_lipoprotein(**args):
+        GFF.write(record, sys.stdout)
