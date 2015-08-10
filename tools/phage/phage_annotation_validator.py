3614b92189189f77654351669365f6941fe783c8
Eric Rasche <rasche.eric@gmail.com>
2015-08-09 19:43:46 -0500

    Document, refactor dashboard css

--- tools/phage/phage_annotation_validator.py
+++ tools/phage/phage_annotation_validator.py
@@ -8,11 +8,11 @@ from jinja2 import Template
 import logging
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
+
+# Path to script, required because of Galaxy.
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
-
+# Path to the HTML template for the report
 REPORT_TEMPLATE = Template(open(os.path.join(SCRIPT_PATH, 'phage_annotation_validator.html'), 'r').read())
-
-dashcss = open(os.path.join(SCRIPT_PATH, 'dashboard.css')).read()
 
 __author__ = "Eric Rasche"
 __version__ = "0.4.0"
@@ -42,24 +42,41 @@ def __ensure_location_in_bounds(start=0, end=0, parent_length=0):
 
 
 def missing_rbs(record, lookahead_min=5, lookahead_max=15):
+    """
+    Identify gene features with missing RBSs
+
+    This "looks ahead" 5-15 bases ahead of each gene feature, and checks if
+    there's an RBS feature in those bounds.
+
+    The returned data is a set of genes with the RBS sequence in the __upstream
+    attribute, and a message in the __message attribute.
+    """
     results = []
     good = 0
     bad = 0
 
     for gene in genes(record.features):
+        # Check if there are RBSs, TODO: make this recursive. Each feature in
+        # gene.sub_features can also have sub_features.
         has_rbs = [x.type == "Shine_Dalgarno_sequence" for x in gene.sub_features]
+        # No RBS found
         if not any(has_rbs):
-            # Shinefind copy pasta to find upstream seq
+            # Get the sequence lookahead_min to lookahead_max upstream
             if gene.strand > 0:
                 start = gene.location.start - lookahead_max
                 end = gene.location.start - lookahead_min
             else:
                 start = gene.location.end + lookahead_min
                 end = gene.location.end + lookahead_max
+            # We have to ensure the feature is ON the genome, otherwise we may
+            # be trying to access a location outside of the length of the
+            # genome, which would be bad.
             (start, end) = __ensure_location_in_bounds(start=start, end=end,
                                                        parent_length=record.__len__)
+            # Temporary feature to extract sequence
             tmp = SeqFeature(FeatureLocation(start, end, strand=gene.strand),
                              type='domain')
+            # Get the sequence
             seq = str(tmp.extract(record.seq))
             gene.__upstream = seq
             gene.__message = "No RBS"
@@ -67,14 +84,17 @@ def missing_rbs(record, lookahead_min=5, lookahead_max=15):
             bad += 1
             results.append(gene)
         else:
+            # get first RBS/CDS
             rbs = [x for x in gene.sub_features if x.type == "Shine_Dalgarno_sequence"][0]
             cds = [x for x in gene.sub_features if x.type == "CDS"][0]
 
+            # Get the distance between the two
             if gene.strand > 0:
                 distance = cds.location.start - rbs.location.end
             else:
                 distance = rbs.location.start - cds.location.end
 
+            # If the RBS is too far away, annotate that
             if distance > lookahead_max:
                 gene.__message = "RBS too far away (%s nt)" % distance
                 bad += 1
@@ -86,15 +106,26 @@ def missing_rbs(record, lookahead_min=5, lookahead_max=15):
 
 
 def excessive_gap(record, excess=10):
+    """
+    Identify excessive gaps between gene features.
+
+    Default "excessive" gap size is 10, but that should likely be larger.
+    """
     results = []
     good = 0
     bad = 0
 
+    # This is a dictionary containing True for every point on the genome that
+    # has a feature covering it. There is almost certainly a better way to do
+    # this, but this is quick&dirty.
+    # TODO: replace with real math rather than bruteforcing.
     annotated_regions = {}
     for gene in genes(record.features):
         for i in range(gene.location.start, gene.location.end):
             annotated_regions[i] = True
 
+    # loop across every base in the genome, if we count a run of Falses, that
+    # indicates a gap, add that to our list of gaps.
     annotated = True
     unannotated_count = 0
     region_start = 0
@@ -118,6 +149,7 @@ def excessive_gap(record, excess=10):
 
             annotated = True
 
+    # Append any remaining regions to our list of regions which are large gaps.
     if not annotated:
         results.append((region_start, i))
 
@@ -130,24 +162,47 @@ def excessive_gap(record, excess=10):
 
 
 def genes(feature_list):
+    """
+    Simple filter to extract gene features from the feature set.
+
+    TODO: not recursive.
+    """
     for x in feature_list:
         if x.type == 'gene':
             yield x
 
 
 def excessive_overlap(record, excessive=15):
+    """
+    Find excessive overlaps in the genome, where excessive is defined as 15
+    bases.
+
+    Does a product of all the top-level features in the genome, and calculates
+    gaps.
+
+    TODO: switch to itertools
+    """
     results = []
     bad = 0
 
     for gene_a in genes(record.features):
         for gene_b in genes(record.features):
+            # If they're not the same feature, and a<b (so we don't compare A+B
+            # and B+A), itertools would fix all of this....
             if gene_a != gene_b and gene_a.location.start <= gene_b.location.start:
+                # Get the CDS from the subfeature list.
+                # TODO: not recursive.
                 cds_a = [x for x in gene_a.sub_features if x.type == "CDS"][0]
                 cds_b = [x for x in gene_b.sub_features if x.type == "CDS"][0]
 
+                # Set of locations that are included in the CDS of A and the
+                # CDS of B
                 cas = set(range(cds_a.location.start, cds_a.location.end))
                 cbs = set(range(cds_b.location.start, cds_b.location.end))
 
+                # Here we calculate the intersection between the two sets, and
+                # if it's larger than our excessive size, we know that they're
+                # overlapped
                 ix = cas.intersection(cbs)
                 if len(ix) >= excessive:
                     bad += 1
@@ -159,6 +214,8 @@ def excessive_overlap(record, excessive=15):
 
 
 def get_encouragement(score):
+    """Some text telling the user how they did
+    """
     for encouragement in ENCOURAGEMENT:
         if score > encouragement[0]:
             return encouragement[1]
@@ -166,6 +223,12 @@ def get_encouragement(score):
 
 
 def find_morons(record):
+    """Locate morons in the genome
+
+    Don't even know why...
+
+    TODO: remove? Idk.
+    """
     results = []
     good = 0
     bad = 0
@@ -194,8 +257,12 @@ def find_morons(record):
 
 
 def evaluate_and_report(annotations, genome):
+    """
+    Generate our HTML evaluation of the genome
+    """
     # Get features from GFF file
     seq_dict = SeqIO.to_dict(SeqIO.parse(genome, "fasta"))
+    # Get the first GFF3 record
     record = list(GFF.parse(annotations, base_dict=seq_dict))[0]
     upstream_min = 5
     upstream_max = 15
@@ -217,8 +284,8 @@ def evaluate_and_report(annotations, genome):
 
     score = int(100 * score_good / (score_bad + score_good))
 
+    # This is data that will go into our HTML template
     kwargs = {
-        'dash': dashcss,
         'upstream_min': upstream_min,
         'upstream_max': upstream_max,
         'record_name': record.id,
@@ -241,9 +308,6 @@ def evaluate_and_report(annotations, genome):
     }
 
     return REPORT_TEMPLATE.render(**kwargs)
-
-
-
 
 
 if __name__ == '__main__':
