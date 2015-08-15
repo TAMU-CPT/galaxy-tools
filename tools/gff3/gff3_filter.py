c89e6fa218c44a286df26ee770403d5f4982076c
Eric Rasche <rasche.eric@gmail.com>
2015-08-14 19:49:15 -0500

    Subfeature filter tool

--- tools/gff3/gff3_filter.py
+++ tools/gff3/gff3_filter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+import copy
 import logging
 import argparse
 from BCBio import GFF
@@ -11,20 +12,45 @@ __maintainer__ = "Eric Rasche"
 __email__ = "esr@tamu.edu"
 
 
-def gff_filter(gff3, attribute_field='ID', filter_list):
+def feature_lambda(feature_list, test, test_kwargs, subfeatures=True):
+    # Either the top level set of [features] or the subfeature attribute
+    for feature in feature_list:
+        if test(feature, **test_kwargs):
+            feature_copy = copy.deepcopy(feature)
+            if not subfeatures:
+                feature_copy.sub_features = []
+            yield feature_copy
+
+        if hasattr(feature, 'sub_features'):
+            for x in feature_lambda(feature.sub_features, test, test_kwargs, subfeatures=subfeatures):
+                yield x
+
+def feature_test(feature, **kwargs):
+    for attribute_value in feature.qualifiers.get(kwargs['qualifier'], []):
+        if attribute_value in kwargs['attribute_list']:
+            log.info(kwargs['attribute_list'])
+            log.info(attribute_value)
+            return True
+    return False
+
+
+def gff_filter(gff3, filter_list, attribute_field='ID', subfeatures=True):
     filter_strings = [line.strip() for line in filter_list]
-    for rec in GFF.parse(parent):
-        rec.features = [
-            feat for feat in rec.features if
-            # if the attribute_value is in the filter_strings list
-            any([attribute_value in filter_strings for attribute_value in feat.get(attribute_field, [])])
-        ]
+    print filter_strings
+    for rec in GFF.parse(gff3):
+        rec.features = feature_lambda(
+            rec.features,
+            feature_test,
+            {'qualifier': attribute_field, 'attribute_list': filter_strings},
+            subfeatures=subfeatures
+        )
         GFF.write([rec], sys.stdout)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='extract features from a GFF3 file based on ID/qualifiers')
     parser.add_argument('gff3', type=file, help='GFF3 annotations')
-    parser.add_argument('attribute_field', type=str, help='Column 9 Field to search against', default='ID')
     parser.add_argument('filter_list', type=file, help='Child GFF3 annotations to rebase against parent')
+    parser.add_argument('--attribute_field', type=str, help='Column 9 Field to search against', default='ID')
+    parser.add_argument('--subfeatures', action='store_true', help='Retain subfeature tree of matched features')
     args = parser.parse_args()
     gff_filter(**vars(args))
