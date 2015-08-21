2e5a6dcc76b4b3e90e08765e3971c626af02dd01
Eric Rasche <rasche.eric@gmail.com>
2015-08-21 14:41:41 -0500

    WIP sequin to gff3 annotation track tool
    CC @gabby-kuty, this will be nice for you whenever I finish it.
    Sequin will be added as a track to JBrowse so it'll be easy to visualize/edit
    things that Genbank complains about

---
+++ tools/gff3/gff3_sequin.py
@@ -0,0 +1,51 @@
+import re
+from Bio.SeqFeature import SeqFeature, FeatureLocation
+
+class SequinReportParser(object):
+
+    def gen_qc_feature(start, end, message, strand=0, level='FATAL'):
+        return SeqFeature(
+            FeatureLocation(start, end, strand=strand),
+            qualifiers={
+                'note': [message],
+                'color': ['#FF0000' if level == 'FATAL' else '#FFFF33'],
+            }
+        )
+
+    def __init__(self, id='Mordin'):
+        self.regex = {
+            # Info?
+            'DiscRep_ALL': re.compile('^DiscRep_ALL:(?P<ID>[^:]*)::(?P<MESSAGE>.*)'),
+            # Info?
+            'DiscRep_SUB': re.compile('^DiscRep_SUB:(?P<ID>[^:]*)::(?P<MESSAGE>.*)'),
+            # Def. an error
+            'DiscRep_Error_Fatal': re.compile('^FATAL: DiscRep_ALL:(?P<ID>[^:]*)::(?P<MESSAGE>.*)'),
+        }
+        self.id = id
+
+
+    def parse_discrep(self, discrep):
+        gff3_qc_record = SeqRecord(self.id, id=self.id)
+        gff3_qc_record.features = []
+
+        with open(discrep, 'r') as handle:
+            detailed = False
+            current_id = None
+            for line in handle:
+                if line.startswith('Detailed Report'):
+                    detailed = True
+
+                if detailed:
+                    for regex_key in self.regex:
+                        m = re.match(self.regex[regex_key], line.strip())
+                        if m:
+                            if regex_key in ('DiscRep_Erorr_Fatal'):
+                                #gen_qc_feature()
+                                ## Error
+                                #print m.group('ID')
+
+
+if __name__ == '__main__':
+    import sys
+    srp = SequinReportParser()
+    srp.parse_discrep(sys.argv[1])
