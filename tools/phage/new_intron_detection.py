28bd30074c390c9c0a4135e53551d002177c6f57
Eleni Mijalis <elenimijalis@gmail.com>
2015-09-24 23:52:39 -0500

    more filtering of clusters

--- tools/phage/new_intron_detection.py
+++ tools/phage/new_intron_detection.py
@@ -2,11 +2,12 @@
 import sys
 import re
 import argparse
+import hashlib
 # from BCBio import GFF
 from Bio.Blast import NCBIXML
 
-""" Parses xml file to get desired info (genes, hits, etc) """
 def parse_xml(blastxml):
+    """ Parses xml file to get desired info (genes, hits, etc) """
     blast = []
     for iter_num, blast_record in enumerate(NCBIXML.parse(blastxml)):
         blast_gene = []
@@ -24,16 +25,16 @@ def parse_xml(blastxml):
         blast.append(blast_gene)
     return blast
 
-""" Removes all clusters with only one member """
 def filter_clusters(matches):
+    """ Removes all clusters with only one member and those with no hits """
     filtered_matches = {}
     for key in matches:
-        if len(matches[key]) > 1:
+        if len(matches[key]) > 1 and len(key) > 0:
             filtered_matches[key] = matches[key]
     return filtered_matches
 
-""" Each gene will have multiple Hits """
 class Hit(object):
+    """ Each gene will have multiple Hits """
     def __init__(self, gi_nos, query_start, query_end, sbjct_start, sbjct_end, iter_def, iter_num):
         self.gi_nos = gi_nos
         self.query_start = query_start
@@ -44,26 +45,26 @@ class Hit(object):
         self.iter_num = iter_num
 
     def __repr__(self):
-        return '[%s], %d, %d, %d, %d' % (','.join(self.gi_nos), self.query_start, self.query_end, self.sbjct_start, self.sbjct_end)
+        return self.iter_def
 
-""" IntronFinder objects are lists that contain a list of Hits for every gene """
 class IntronFinder(object):
+    """ IntronFinder objects are lists that contain a list of Hits for every gene """
     def __init__(self, genes, blastp):
         self.blast = []
         self.matches = {}
         self.blast = parse_xml(blastp)
 
-    """ Finds 2 or more genes with matching hits """
     def create_clusters(self):
+        """ Finds 2 or more genes with matching hits """
         clusters = {}
         for gene in self.blast:
             for hit in gene:
-                name = ','.join(hit.gi_nos)
+                name = hashlib.md5(','.join(hit.gi_nos)).hexdigest()
                 if name in clusters:
-                    if hit.iter_def not in clusters[name]:
-                        clusters[name].append(hit.iter_def)
+                    if hit not in clusters[name]:
+                        clusters[name].append(hit)
                 else:
-                    clusters[name] = [hit.iter_def]
+                    clusters[name] = [hit]
         self.matches = filter_clusters(clusters)
 
         # import pprint; pprint.pprint(self.matches)
@@ -72,8 +73,8 @@ class IntronFinder(object):
     # def check_strand():
     # def check_distance():
 
-    """ merge 2 or more seq records into one """
     # def modify_gff3(?):
+    """ merge 2 or more seq records into one """
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Intron detection')
@@ -81,10 +82,9 @@ if __name__ == '__main__':
     parser.add_argument('blastp', type=file, help='blast XML protein results')
     args = parser.parse_args()
 
-    """ create new IntronFinder object based on user input """
+    # create new IntronFinder object based on user input
     ifinder = IntronFinder(**vars(args))
     ifinder.create_clusters()
-    for idx, key in enumerate(ifinder.matches):
-        if idx > 3:
-            sys.exit()
-        print ifinder.matches[key]
+
+    with open('out.txt', 'w') as handle:
+        import pprint; pprint.pprint(ifinder.matches)
