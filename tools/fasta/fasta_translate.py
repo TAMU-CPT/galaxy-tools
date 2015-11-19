71e2871d58aadef78d161f10bb782842cc9ce273
Eric Rasche <rasche.eric@gmail.com>
2015-11-19 16:03:00 -0600

    Support RNA

--- tools/fasta/fasta_translate.py
+++ tools/fasta/fasta_translate.py
@@ -6,7 +6,7 @@ from Bio import SeqIO
 import StringIO
 
 
-def translate(fasta_file, table=11, strip_stops=False):
+def translate(fasta_file, target='protein', table=11, strip_stops=False):
     records = list(SeqIO.parse(fasta_file, "fasta"))
     output = StringIO.StringIO()
 
@@ -15,11 +15,15 @@ def translate(fasta_file, table=11, strip_stops=False):
     strip_stops_diff = 0 if strip_stops else -1
 
     for record in records:
-        tmpseq = record.seq.translate(table=table)
-        if '*' in tmpseq:
-            tmpseq = tmpseq[0:str(tmpseq).index('*') - strip_stops_diff]
-        record.seq = tmpseq
-        if len(record.seq) > 0:
+        if target == 'protein':
+            tmpseq = record.seq.translate(table=table)
+            if '*' in tmpseq:
+                tmpseq = tmpseq[0:str(tmpseq).index('*') - strip_stops_diff]
+            record.seq = tmpseq
+            if len(record.seq) > 0:
+                SeqIO.write(record, output, "fasta")
+        else:
+            record.seq = record.seq.transcribe()
             SeqIO.write(record, output, "fasta")
 
     print output.getvalue()
@@ -27,6 +31,7 @@ def translate(fasta_file, table=11, strip_stops=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Translate fasta file')
     parser.add_argument('fasta_file', type=file, help='Fasta file')
+    parser.add_argument('--target', choices=['protein', 'rna'])
     parser.add_argument('--table', type=int, default=11,
                         help='Translation table to use', choices=range(1, 23))
     parser.add_argument('--strip_stops', action='store_true', help='Remove stop characters')
