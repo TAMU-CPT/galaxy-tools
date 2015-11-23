a2bbe4aee02b6b7fa63780a74a9ecee443260e12
Eric Rasche <rasche.eric@gmail.com>
2015-11-23 10:12:27 -0600

    Allow seq_export to play nicely with JBrowse

--- tools/gbk/seq_export.py
+++ tools/gbk/seq_export.py
@@ -11,8 +11,11 @@ log = logging.getLogger()
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Export sequences from Genbank files')
     parser.add_argument('genbank_files', nargs='+', type=file, help='Genbank file')
+    parser.add_argument('--use_name', action='store_true')
     args = parser.parse_args()
 
     for gbk in args.genbank_files:
         for seq in SeqIO.parse(gbk, 'genbank'):
+            if args.use_name:
+                seq.id = seq.name
             SeqIO.write(seq, sys.stdout, 'fasta')
