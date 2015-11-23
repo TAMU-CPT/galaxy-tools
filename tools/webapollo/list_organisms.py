010cda52564665a7130c9c6af877c41f75374171
Eric Rasche <esr@tamu.edu>
2015-11-23 15:24:30 -0600

    Changes for production

---
+++ tools/webapollo/list_organisms.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+import json
+import argparse
+from webapollo import WebApolloInstance
+
+if __name__ == '__main__':
+    json
+    parser = argparse.ArgumentParser(description='Sample script to add an attribute to a feature via web services')
+    parser.add_argument('apollo', help='Complete Apollo URL')
+    parser.add_argument('username', help='WA Username')
+    parser.add_argument('password', help='WA Password')
+
+    args = parser.parse_args()
+
+    wa = WebApolloInstance(args.apollo, args.username, args.password)
+    json.dump(wa.organisms.findAllOrganisms(), sys.stdout)
+
