f45a2721120c7959485fdc415fd4c02a4229957f
Eric Rasche <rasche.eric@gmail.com>
2015-12-10 11:09:48 -0600

    Tool to require SD

---
+++ tools/gff3/gff3_require_sd.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import sys
+import logging
+import argparse
+from Bio import SeqIO
+from BCBio import GFF
+from gff3 import feature_lambda, feature_test_type
+from shinefind import NaiveSDCaller
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+def require_shinefind(gff3, fasta):
+    sd_finder = NaiveSDCaller()
+    # Load up sequence(s) for GFF3 data
+    seq_dict = SeqIO.to_dict(SeqIO.parse(fasta, "fasta"))
+    # Parse GFF3 records
+    for record in GFF.parse(gff3, base_dict=seq_dict):
+        # Reopen
+        genes = list(feature_lambda(record.features, feature_test_type, {'type': 'CDS'}, subfeatures=True))
+        good_genes = []
+        for gene in genes:
+            if sd_finder.hasSd(gene, record):
+                good_genes.append(gene)
+
+        # Yuck!
+        record.features = good_genes
+            # log.warn("WARNING: Index chosen is in the middle of a feature. This feature will disappear from the output")
+        # log.debug(rec.annotations)
+        # # TODO: This call removes metadata!
+        # rec = rec[index:] + rec[0:index]
+        # log.debug(rec.annotations)
+        yield record
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Identify shine-dalgarno sequences')
+    parser.add_argument('fasta', type=file, help='Fasta Genome')
+    parser.add_argument('gff3', type=file, help='GFF3 annotations')
+    args = parser.parse_args()
+
+    for rec in require_shinefind(**vars(args)):
+        GFF.write([rec], sys.stdout)
