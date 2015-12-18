bcb2e220bf20b775dfad9b8dc19d393b937d44d1
Eric Rasche <rasche.eric@gmail.com>
2015-12-18 12:26:33 -0600

    Add untested revcom tool

---
+++ tools/gbk/revcom.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import argparse
+import sys
+from Bio import SeqIO
+
+import logging
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='RevCom genbank files')
+    parser.add_argument('gbk_files', type=file, nargs='+', help='Genbank files')
+
+    args = parser.parse_args()
+    for gbk_file in args.gbk_files:
+        for record in SeqIO.parse(gbk_file, 'genbank'):
+            SeqIO.write(record.reverse_complement(id=True, name=True, description=True, features=True, annotations=True, letter_annotations=True, dbxrefs=True), sys.stdout, 'genbank')
