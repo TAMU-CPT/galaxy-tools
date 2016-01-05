306544eb39fd81595226169a179ba764484be6ea
Eric Rasche <rasche.eric@gmail.com>
2016-01-05 11:48:13 -0600

    Add tool to correct genermarks gene model

---
+++ tools/gff3/gff3_fix_genemarks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import copy
+import logging
+import argparse
+from BCBio import GFF
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+
+def fix_genemarks(gff3):
+    for rec in GFF.parse(gff3):
+        new_features = []
+        for feature in rec.features:
+            gene_parent = copy.deepcopy(feature)
+            gene_parent.type = 'gene'
+            feature.qualifiers['ID'][0] = feature.qualifiers['ID'][0].replace('gene', 'cds')
+            gene_parent.sub_features = [feature]
+            del gene_parent.qualifiers['score']
+            del gene_parent.qualifiers['phase']
+            new_features.append(gene_parent)
+        rec.features = new_features
+        GFF.write([rec], sys.stdout)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='correct their terrible GFF data')
+    parser.add_argument('gff3', type=file, help='GFF3 annotations')
+    args = parser.parse_args()
+    fix_genemarks(**vars(args))
