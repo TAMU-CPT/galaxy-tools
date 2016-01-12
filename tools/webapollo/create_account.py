8b1e108214efb12dfa2050f294c2128e1ee2da28
Eric Rasche <rasche.eric@gmail.com>
2016-01-12 14:42:14 -0600

    Add script to create account

---
+++ tools/webapollo/create_account.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import json
+import argparse
+from webapollo import WebApolloInstance
+
+def pwgen(length):
+    chars = list('qwrtpsdfghjklzxcvbnm')
+    return ''.join(choice(chars) for _ in range(length))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Sample script to add an account via web services')
+    parser.add_argument('apollo', help='Complete Apollo URL')
+    parser.add_argument('username', help='WA Admin Username')
+    parser.add_argument('password', help='WA Admin Password')
+
+    parser.add_argument('email', help='User Email')
+    parser.add_argument('--first', help='First Name', default='J')
+    parser.add_argument('--last', help='Last Name', default='Aggie')
+    args = parser.parse_args()
+
+    wa = WebApolloInstance(args.apollo, args.username, args.password)
+
+    password = pwgen(12)
+    wa.users.createUser(args.email, args.first, args.last, password, role='user')
+    print 'Username: %s\nEmail: %s' % (args.email, password)
