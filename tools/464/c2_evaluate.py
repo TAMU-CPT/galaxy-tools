76c80a742204cc42e2363a12f7d3b29f325bd890
Eric Rasche <rasche.eric@gmail.com>
2016-02-13 13:42:59 -0600

    Add c2 eval script

---
+++ tools/464/c2_evaluate.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import argparse
+import subprocess
+import logging
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+from guanine import GuanineClient
+g = GuanineClient()
+
+def validate(official_data, user_data, user_email):
+    od = subprocess.check_output(['md5sum', official_data])
+    ud = subprocess.check_output(['md5sum', user_data])
+
+    od = od[0:32]
+    ud = ud[0:32]
+
+    if od == ud:
+        print 'CORRECT'
+        g.submit(user_email, 'C2', 1)
+    else:
+        print 'INCORRECT'
+        g.submit(user_email, 'C2', 0)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='verify against expectations')
+    parser.add_argument('official_data')
+    parser.add_argument('user_data')
+    parser.add_argument('user_email')
+    args = parser.parse_args()
+    validate(**vars(args))
