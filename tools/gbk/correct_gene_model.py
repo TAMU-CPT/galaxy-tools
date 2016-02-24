a279e2cf370ad96ffbfd54f480c411dcbe571896
Eric Rasche <rasche.eric@gmail.com>
2016-02-24 11:54:30 -0600

    Enahnced gene model corrector

--- tools/gbk/correct_gene_model.py
+++ tools/gbk/correct_gene_model.py
@@ -4,51 +4,86 @@ import argparse
 from Bio import SeqIO
 # from Bio.Seq import Seq
 # from Bio.SeqRecord import SeqRecord
-from Bio.SeqFeature import SeqFeature
-from logger import notify
+from Bio.SeqFeature import SeqFeature, FeatureLocation
 
 import logging
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger()
 
 
+def nearbyRbss(cds, rbss):
+    for r in rbss:
+        if cds.strand > 0:
+            return [r for r in rbss if
+                    abs(r.location.end - cds.location.start) < 20
+                    and r.location.strand == cds.location.strand]
+        else:
+            return [r for r in rbss if
+                    abs(r.location.start - cds.location.end) < 20
+                    and r.location.strand == cds.location.strand]
+
+def unionLoc(a, b):
+    return FeatureLocation(
+        start=min(a.start, b.start),
+        end=max(a.end, b.end),
+        strand=a.strand
+    )
+
 def correct_model(genbank_file):
     for record in SeqIO.parse(genbank_file, "genbank"):
         cds = [f for f in record.features if f.type == 'CDS']
         genes = [f for f in record.features if f.type == 'gene']
+        rbss = [f for f in record.features if f.type == 'RBS']
 
         # No genes at all
         if len(genes) == 0:
-            genes = [
-                SeqFeature(
-                    f.location,
-                    type="gene",
-                    strand=f.location.strand,
-                    qualifiers={
-                        'gene': f.qualifiers.get('gene', []),
-                        'product': f.qualifiers.get('product', []),
-                        'protein_id': f.qualifiers.get('protein_id', []),
-                        'locus_tag': f.qualifiers.get('locus_tag', []),
-                    }
-                )
-                for f in cds
-            ]
+            if len(rbss) > 0:
+                extra_feats = []
+                for c in cds:
+                    r = nearbyRbss(c, rbss)
+                    print r
+                    if len(r) == 1:
+                        extra_feats.append(SeqFeature(
+                            unionLoc(c.location, r[0].location),
+                            type="gene",
+                            qualifiers={
+                                'gene': f.qualifiers.get('gene', []),
+                                'product': f.qualifiers.get('product', []),
+                                'protein_id': f.qualifiers.get('protein_id', []),
+                                'locus_tag': f.qualifiers.get('locus_tag', []),
+                            }
+                        ))
+                    else:
+                        extra_feats.append(SeqFeature(
+                            c.location,
+                            type="gene",
+                            qualifiers={
+                                'gene': f.qualifiers.get('gene', []),
+                                'product': f.qualifiers.get('product', []),
+                                'protein_id': f.qualifiers.get('protein_id', []),
+                                'locus_tag': f.qualifiers.get('locus_tag', []),
+                            }
+                        ))
+                record.features.extend(extra_feats)
+            else:
+                genes = [
+                    SeqFeature(
+                        f.location,
+                        type="gene",
+                        strand=f.location.strand,
+                        qualifiers={
+                            'gene': f.qualifiers.get('gene', []),
+                            'product': f.qualifiers.get('product', []),
+                            'protein_id': f.qualifiers.get('protein_id', []),
+                            'locus_tag': f.qualifiers.get('locus_tag', []),
+                        }
+                    )
+                    for f in cds
+                ]
             # print genes
             record.features += genes
         elif len(genes) != len(cds):
-            log.warn("Different number of CDSs and genes. I don't know how to handle this case (yet)")
-            import StringIO
-            # Eh? This is odd.
-            c = StringIO.StringIO()
-            SeqIO.write([record], c, 'genbank')
-            notify(
-                'correct_gene_model found something interesting',
-                'Genes: %s, CDSs: %s' % (len(genes), len(cds)),
-                {
-                    'content': c.getvalue(),
-                    'filename': '%s.gbk' % record.id,
-                }
-            )
+            log.error("Different number of CDSs and genes. I don't know how to handle this case (yet)")
 
         record.features = sorted(record.features, key=lambda x: x.location.start)
         yield [record]
