00791a46b0af7f420b44e3fe0e37a8afddb46432
Eric Rasche <rasche.eric@gmail.com>
2016-03-30 05:46:11 +0000

    First test of frameshift tool

---
+++ tools/phage/frameshift.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+import argparse
+import re
+import sys
+from BCBio import GFF
+from Bio import SeqIO
+from Bio.SeqFeature import SeqFeature, FeatureLocation
+from gff3 import feature_lambda, feature_test_type
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger()
+
+
+def FrameShiftFinder(gff3, fasta, max_overlap=60, table=11, slippage_max=-3):
+    seq_dict = SeqIO.to_dict(SeqIO.parse(fasta, "fasta"))
+    for rec in GFF.parse(gff3, base_dict=seq_dict):
+        putative_frameshift_genes = []
+
+        for gene in feature_lambda(rec.features, feature_test_type,
+                {'type': 'gene'}):
+            feats = list(feature_lambda(gene.sub_features, feature_test_type, {'type': 'CDS'}))
+            if len(feats) == 0:
+                continue
+
+            # only the first CDS. Bad idea...
+            feat = feats[0]
+
+            # For each feature, we check the last N bases
+            overlap = min(len(feat), max_overlap)
+            # Make sure overlap is evenly divisible by three
+            if overlap % 3 != 0:
+                overlap -= (overlap % 3)
+
+            feat_seq = feat.extract(rec)
+            # Loop across the last overlap / 3 codons
+            for idx in range(len(feat) - overlap, len(feat), 3):
+                codons = feat_seq[idx - 6:idx]
+                # Look from -10 to +5, and check under wobble rules, one
+                # or two codons are equal.
+                tn_codons = codons.seq.translate(table=table)
+                for wobble in range(slippage_max, 0):
+                    cmp_codons = feat_seq[idx - 6 + wobble:idx+wobble]
+                    cmp_codons = cmp_codons.seq.translate(table=table)
+
+                    if tn_codons != cmp_codons:
+                        continue
+
+                    # Here we've found a feature which has a possible
+                    # frame shift target. Now we need to check if there's
+                    # a run of stop-codon free.
+                    if feat.location.strand > 0:
+                        possible_frameshift_start = feat.location.start + \
+                            idx - 6 + wobble
+                        frameshift_to_end = rec.seq[possible_frameshift_start:]
+                    else:
+                        possible_frameshift_start = feat.location.end - \
+                            idx + 6 - wobble
+                        frameshift_to_end = rec.seq[0:possible_frameshift_start].reverse_complement()
+
+                    translated_seq = frameshift_to_end.translate(table=11, to_stop=True)
+
+                    # Skip sequences which are too short. Divide by three
+                    # as trnaslated seq is protein, and feat is DNA.
+                    # Divide by 2 again so we collect sequences which are
+                    # at least half as long as the parent.
+                    if len(translated_seq) < len(feat) / (3 * 2):
+                        continue
+
+                    # Ok, we need to add a new mRNA structure for this
+                    if feat.location.strand > 0:
+                        mRNAstart = feat.location.start
+                        mRNAend = possible_frameshift_start + (len(translated_seq) * 3)
+                    else:
+                        mRNAend = feat.location.end
+                        mRNAstart = possible_frameshift_start - (len(translated_seq) * 3)
+
+                    mRNA = SeqFeature(
+                        FeatureLocation(
+                            mRNAstart,
+                            mRNAend,
+                            strand=feat.location.strand
+                        ),
+                        type='mRNA',
+                        qualifiers={
+                            'source': ['CPT_FSFinder'],
+                        }
+                    )
+                    exon = SeqFeature(
+                        FeatureLocation(
+                            mRNAstart,
+                            mRNAend,
+                            strand=feat.location.strand
+                        ),
+                        type='exon',
+                        qualifiers={
+                            'source': ['CPT_FSFinder'],
+                        }
+                    )
+
+                    # CDSs are more complex/annoying
+                    if feat.location.strand > 0:
+                        cdsA_start = feat.location.start
+                        cdsA_end = feat.location.start + idx - 6
+                    else:
+                        cdsA_end = feat.location.end
+                        cdsA_start = feat.location.end - idx + 6
+                    cds_a = SeqFeature(
+                        FeatureLocation(
+                            cdsA_start,
+                            cdsA_end,
+                            strand=feat.location.strand
+                        ),
+                        type='CDS',
+                        qualifiers={
+                            'source': ['CPT_FSFinder'],
+                            'phase': [0],
+                        }
+                    )
+
+                    # CDS B = eww
+                    if feat.location.strand > 0:
+                        cdsB_start = possible_frameshift_start
+                        cdsB_end = possible_frameshift_start + (len(translated_seq) * 3)
+                    else:
+                        cdsB_end = possible_frameshift_start
+                        cdsB_start = possible_frameshift_start - (len(translated_seq) * 3)
+                    cds_b = SeqFeature(
+                        FeatureLocation(
+                            cdsB_start,
+                            cdsB_end,
+                            strand=feat.location.strand
+                        ),
+                        type='CDS',
+                        qualifiers={
+                            'source': ['CPT_FSFinder'],
+                            'phase': [wobble % 3], # WHO KNOWS
+                        }
+                    )
+
+                    print feat.id, feat.location.strand, \
+                            possible_frameshift_start, idx, wobble, codons.seq, tn_codons, cmp_codons, frameshift_to_end[0:6]
+                    print translated_seq
+
+                    exon.sub_features = [cds_a, cds_b]
+                    mRNA.sub_features = [exon]
+                    gene.sub_features = [mRNA]
+                    putative_frameshift_genes.append(gene)
+
+        rec.features = putative_frameshift_genes
+        yield [rec]
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('fasta', type=file, help='FASTA genome calls')
+    parser.add_argument('gff3', type=file, help='GFF3 gene calls')
+    parser.add_argument('--max_overlap', type=int, default=60)
+    parser.add_argument('--table', type=int, default=11)
+
+    args = parser.parse_args()
+    for rec in FrameShiftFinder(**vars(args)):
+        GFF.write(rec, sys.stdout)
