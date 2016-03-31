0980edd552df03cc6aa981a6fc9e73de4d71e368
Eric Rasche <rasche.eric@gmail.com>
2016-03-31 17:28:18 +0000

    Forgot args

--- tools/gff3/remove_annotations.py
+++ tools/gff3/remove_annotations.py
@@ -6,8 +6,8 @@ from BCBio import GFF
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('gff3', type=file, help='GFF3 annotations')
+    args = parser.parse_args()
 
     for rec in GFF.parse(args.gff3):
         rec.annotations = {}
-
         GFF.write([rec], sys.stdout)
