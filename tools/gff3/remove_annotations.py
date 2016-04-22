101ecb78625b635f76ec8cb070567bef55a8f7f7
Eric Rasche <rasche.eric@gmail.com>
2016-04-22 21:37:41 +0000

    Add a remark flag

--- tools/gff3/remove_annotations.py
+++ tools/gff3/remove_annotations.py
@@ -6,8 +6,11 @@ from BCBio import GFF
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('gff3', type=file, help='GFF3 annotations')
+    parser.add_argument('--remark', action='store_true', help='Remove remark features')
     args = parser.parse_args()
 
     for rec in GFF.parse(args.gff3):
         rec.annotations = {}
+        if args.remark:
+            rec.features = [x for x in rec.features if x.type != 'remark']
         GFF.write([rec], sys.stdout)
