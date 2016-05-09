5a85e6f69636b2e76d5fe463e0f33395a3d95f5b
Eric Rasche <rasche.eric@gmail.com>
2016-05-09 17:02:14 +0000

    Support multiple orgs on backend

--- tools/webapollo/export.py
+++ tools/webapollo/export.py
@@ -6,6 +6,9 @@ import argparse
 from Bio import SeqIO
 from BCBio import GFF
 from webapollo import WebApolloInstance
+import logging
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     json
@@ -16,29 +19,30 @@ if __name__ == '__main__':
 
     parser.add_argument('commonName', nargs='+', help='Sequence Unique Names')
 
-    parser.add_argument('--gff', type=argparse.FileType('w'))
-    parser.add_argument('--fasta', type=argparse.FileType('w'))
+    parser.add_argument('--gff', type=argparse.FileType('a'), default='out.gff3')
+    parser.add_argument('--fasta', type=argparse.FileType('a'), default='out.fa')
 
     args = parser.parse_args()
 
     wa = WebApolloInstance(args.apollo, args.username, args.password)
 
-    data = StringIO.StringIO(wa.io.write(
-        exportType='GFF3',
-        seqType='genomic',
-        exportAllSequences=False,
-        exportGff3Fasta=True,
-        output="text",
-        exportFormat="text",
-        # TODO: CPT specific convention!!!!!!!!
-        organism=args.commonName,
-        sequences=args.commonName
-    ))
-    data.seek(0)
+    for cn in args.commonName:
+        log.info("Fetching %s" % cn)
+        data = StringIO.StringIO(wa.io.write(
+            exportType='GFF3',
+            seqType='genomic',
+            exportAllSequences=False,
+            exportGff3Fasta=True,
+            output="text",
+            exportFormat="text",
+            # TODO: CPT specific convention!!!!!!!!
+            organism=[cn],
+            sequences=[cn],
+        ).encode('utf-8'))
+        data.seek(0)
 
-    for record in GFF.parse(data):
-        record.annotations = {}
-        GFF.write([record], args.gff)
-        record.description = ""
-        SeqIO.write([record], args.fasta, 'fasta')
-        sys.exit()
+        for record in GFF.parse(data):
+            record.annotations = {}
+            GFF.write([record], args.gff)
+            record.description = ""
+            SeqIO.write([record], args.fasta, 'fasta')
