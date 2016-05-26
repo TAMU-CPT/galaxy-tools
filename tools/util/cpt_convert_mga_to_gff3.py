c5fb581ec9d0ee694324b7b7bc48fa52a2ba7d34
Eric Rasche <rasche.eric@gmail.com>
2016-05-26 16:07:30 +0000

    Fix MGA's support for multiple sequences

--- tools/util/cpt_convert_mga_to_gff3.py
+++ tools/util/cpt_convert_mga_to_gff3.py
@@ -10,69 +10,86 @@ logging.basicConfig(level=logging.INFO)
 
 
 def mga_to_gff3(mga_output, genome):
-    for rec in SeqIO.parse(genome, 'fasta'):
-        for line in mga_output:
-            if not line.startswith('#'):
-                (gene_id, start, end, strand, phase, complete, score, model,
-                    rbs_start, rbs_end, rbs_score) = line.strip().split('\t')
-                start = int(start)
-                end = int(end)
-                strand = +1 if strand == '+' else -1
+    seq_dict = SeqIO.to_dict(SeqIO.parse(genome, "fasta"))
 
-                # Correct for gff3
-                start -= 1
+    output_records = []
+    current_record = None
+    for line in mga_output:
+        if line.startswith('#'):
+            if line.startswith('# gc = '):
+                continue
+            elif line.startswith('# self:'):
+                continue
 
-                rbs_feat = None
-                if rbs_start != '-':
-                    rbs_start = int(rbs_start)
-                    rbs_end = int(rbs_end)
-                    rbs_feat = SeqFeature(
-                        FeatureLocation(rbs_start, rbs_end),
-                        type="Shine_Dalgarno_sequence",
-                        strand=strand,
-                        qualifiers={
-                            'ID': 'rbs_%s' % gene_id,
-                            'Source': 'MGA'
-                        }
-                    )
+            chromId = line.strip().replace('# ', '')
+            if chromId in seq_dict:
+                if current_record is not None:
+                    yield current_record
+                current_record = seq_dict[chromId]
+            else:
+                raise Exception("Found MGA results for sequence %s which was not in fasta file sequences (%s)" % (chromId, ', '.join(seq_dict.keys())))
 
-                cds_feat = SeqFeature(
-                    FeatureLocation(start, end),
-                    type="CDS",
+        else:
+            (gene_id, start, end, strand, phase, complete, score, model,
+                rbs_start, rbs_end, rbs_score) = line.strip().split('\t')
+            start = int(start)
+            end = int(end)
+            strand = +1 if strand == '+' else -1
+
+            # Correct for gff3
+            start -= 1
+
+            rbs_feat = None
+            if rbs_start != '-':
+                rbs_start = int(rbs_start)
+                rbs_end = int(rbs_end)
+                rbs_feat = SeqFeature(
+                    FeatureLocation(rbs_start, rbs_end),
+                    type="Shine_Dalgarno_sequence",
                     strand=strand,
                     qualifiers={
-                        'Source': 'MGA',
-                        'ID': 'cds_%s' % gene_id
+                        'ID': 'rbs_%s' % gene_id,
+                        'Source': 'MGA'
                     }
                 )
 
-                if rbs_feat is not None:
-                    if strand > 0:
-                        gene_start = rbs_start
-                        gene_end = end
-                    else:
-                        gene_start = start
-                        gene_end = rbs_end
+            cds_feat = SeqFeature(
+                FeatureLocation(start, end),
+                type="CDS",
+                strand=strand,
+                qualifiers={
+                    'Source': 'MGA',
+                    'ID': 'cds_%s' % gene_id
+                }
+            )
+
+            if rbs_feat is not None:
+                if strand > 0:
+                    gene_start = rbs_start
+                    gene_end = end
                 else:
                     gene_start = start
-                    gene_end = end
+                    gene_end = rbs_end
+            else:
+                gene_start = start
+                gene_end = end
 
-                gene = SeqFeature(
-                    FeatureLocation(gene_start, gene_end),
-                    type="gene",
-                    strand=strand,
-                    qualifiers={
-                        'Source': 'MGA',
-                        'ID': gene_id
-                    }
-                )
+            gene = SeqFeature(
+                FeatureLocation(gene_start, gene_end),
+                type="gene",
+                strand=strand,
+                qualifiers={
+                    'Source': 'MGA',
+                    'ID': gene_id
+                }
+            )
 
-                gene.sub_features = [cds_feat]
-                if rbs_feat is not None:
-                    gene.sub_features.append(rbs_feat)
+            gene.sub_features = [cds_feat]
+            if rbs_feat is not None:
+                gene.sub_features.append(rbs_feat)
 
-                rec.features.append(gene)
-        yield rec
+            current_record.features.append(gene)
+    yield current_record
 
 
 if __name__ == '__main__':
