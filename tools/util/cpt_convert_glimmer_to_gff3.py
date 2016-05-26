64d4d8e3cd743744c506e3556012e4f285aba4b6
Eric Rasche <rasche.eric@gmail.com>
2016-05-26 15:45:10 +0000

    Support multi fasta

--- tools/util/cpt_convert_glimmer_to_gff3.py
+++ tools/util/cpt_convert_glimmer_to_gff3.py
@@ -10,48 +10,47 @@ logging.basicConfig(level=logging.INFO)
 
 
 def glimmer3_to_gff3(glimmer, genome):
-    rec = SeqIO.read(genome, 'fasta')
+    for rec in SeqIO.parse(genome, 'fasta')
+        for line in glimmer:
+            if not line.startswith('>'):
+                (id, gstart, gend, phase, score) = line.strip().split()
+                gstart = int(gstart)
+                gend = int(gend)
 
-    for line in glimmer:
-        if not line.startswith('>'):
-            (id, gstart, gend, phase, score) = line.strip().split()
-            gstart = int(gstart)
-            gend = int(gend)
+                if '+' in phase:
+                    strand = 1
+                    start = gstart
+                    end = gend
+                else:
+                    strand = -1
+                    start = gend
+                    end = gstart
 
-            if '+' in phase:
-                strand = 1
-                start = gstart
-                end = gend
-            else:
-                strand = -1
-                start = gend
-                end = gstart
+                # Correct for gff3
+                start -= 1
 
-            # Correct for gff3
-            start -= 1
+                cds_feat = SeqFeature(
+                    FeatureLocation(start, end),
+                    type="CDS",
+                    strand=strand,
+                    qualifiers={
+                        'source': 'glimmer',
+                        'ID': 'cds_%s' % id
+                    }
+                )
 
-            cds_feat = SeqFeature(
-                FeatureLocation(start, end),
-                type="CDS",
-                strand=strand,
-                qualifiers={
-                    'source': 'glimmer',
-                    'ID': 'cds_%s' % id
-                }
-            )
-
-            gene = SeqFeature(
-                FeatureLocation(start, end),
-                type="gene",
-                strand=strand,
-                qualifiers={
-                    'source': 'glimmer',
-                    'ID': id,
-                }
-            )
-            gene.sub_features = [cds_feat]
-            rec.features.append(gene)
-    return rec
+                gene = SeqFeature(
+                    FeatureLocation(start, end),
+                    type="gene",
+                    strand=strand,
+                    qualifiers={
+                        'source': 'glimmer',
+                        'ID': id,
+                    }
+                )
+                gene.sub_features = [cds_feat]
+                rec.features.append(gene)
+        yield rec
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert Glimmer to GFF3')
@@ -59,6 +58,5 @@ if __name__ == '__main__':
     parser.add_argument('genome', type=file, help='Fasta Genome')
     args = parser.parse_args()
 
-    result = glimmer3_to_gff3(**vars(args))
-    result.annotations = {}
-    GFF.write([result], sys.stdout)
+    for result in glimmer3_to_gff3(**vars(args)):
+        GFF.write([result], sys.stdout)
--- tools/util/cpt_convert_mga_to_gff3.py
+++ tools/util/cpt_convert_mga_to_gff3.py
@@ -10,70 +10,69 @@ logging.basicConfig(level=logging.INFO)
 
 
 def mga_to_gff3(mga_output, genome):
-    rec = SeqIO.read(genome, 'fasta')
+    for rec in SeqIO.parse(genome, 'fasta'):
+        for line in mga_output:
+            if not line.startswith('#'):
+                (gene_id, start, end, strand, phase, complete, score, model,
+                    rbs_start, rbs_end, rbs_score) = line.strip().split('\t')
+                start = int(start)
+                end = int(end)
+                strand = +1 if strand == '+' else -1
 
-    for line in mga_output:
-        if not line.startswith('#'):
-            (gene_id, start, end, strand, phase, complete, score, model,
-                rbs_start, rbs_end, rbs_score) = line.strip().split('\t')
-            start = int(start)
-            end = int(end)
-            strand = +1 if strand == '+' else -1
+                # Correct for gff3
+                start -= 1
 
-            # Correct for gff3
-            start -= 1
+                rbs_feat = None
+                if rbs_start != '-':
+                    rbs_start = int(rbs_start)
+                    rbs_end = int(rbs_end)
+                    rbs_feat = SeqFeature(
+                        FeatureLocation(rbs_start, rbs_end),
+                        type="Shine_Dalgarno_sequence",
+                        strand=strand,
+                        qualifiers={
+                            'ID': 'rbs_%s' % gene_id,
+                            'Source': 'MGA'
+                        }
+                    )
 
-            rbs_feat = None
-            if rbs_start != '-':
-                rbs_start = int(rbs_start)
-                rbs_end = int(rbs_end)
-                rbs_feat = SeqFeature(
-                    FeatureLocation(rbs_start, rbs_end),
-                    type="Shine_Dalgarno_sequence",
+                cds_feat = SeqFeature(
+                    FeatureLocation(start, end),
+                    type="CDS",
                     strand=strand,
                     qualifiers={
-                        'ID': 'rbs_%s' % gene_id,
-                        'Source': 'MGA'
+                        'Source': 'MGA',
+                        'ID': 'cds_%s' % gene_id
                     }
                 )
 
-            cds_feat = SeqFeature(
-                FeatureLocation(start, end),
-                type="CDS",
-                strand=strand,
-                qualifiers={
-                    'Source': 'MGA',
-                    'ID': 'cds_%s' % gene_id
-                }
-            )
-
-            if rbs_feat is not None:
-                if strand > 0:
-                    gene_start = rbs_start
-                    gene_end = end
+                if rbs_feat is not None:
+                    if strand > 0:
+                        gene_start = rbs_start
+                        gene_end = end
+                    else:
+                        gene_start = start
+                        gene_end = rbs_end
                 else:
                     gene_start = start
-                    gene_end = rbs_end
-            else:
-                gene_start = start
-                gene_end = end
+                    gene_end = end
 
-            gene = SeqFeature(
-                FeatureLocation(gene_start, gene_end),
-                type="gene",
-                strand=strand,
-                qualifiers={
-                    'Source': 'MGA',
-                    'ID': gene_id
-                }
-            )
+                gene = SeqFeature(
+                    FeatureLocation(gene_start, gene_end),
+                    type="gene",
+                    strand=strand,
+                    qualifiers={
+                        'Source': 'MGA',
+                        'ID': gene_id
+                    }
+                )
 
-            gene.sub_features = [cds_feat]
-            if rbs_feat is not None:
-                gene.sub_features.append(rbs_feat)
+                gene.sub_features = [cds_feat]
+                if rbs_feat is not None:
+                    gene.sub_features.append(rbs_feat)
 
-            rec.features.append(gene)
-    return rec
+                rec.features.append(gene)
+        yield rec
 
 
 if __name__ == '__main__':
@@ -82,7 +81,5 @@ if __name__ == '__main__':
     parser.add_argument('genome', type=file, help='Fasta Genome')
     args = parser.parse_args()
 
-    result = mga_to_gff3(**vars(args))
-
-    result.annotations = {}
-    GFF.write([result], sys.stdout)
+    for result in mga_to_gff3(**vars(args)):
+        GFF.write([result], sys.stdout)
