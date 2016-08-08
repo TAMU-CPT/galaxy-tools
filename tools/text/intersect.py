12a406b1de63e46bd7538a253be40948eefb2a97
Eric Rasche <rasche.eric@gmail.com>
2016-08-08 18:34:28 +0000

    Working intersection tool

--- tools/text/intersect.py
+++ tools/text/intersect.py
@@ -5,21 +5,33 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger()
 
 
+def joinStripRow(row):
+    return '\t'.join(row).strip()
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('a', type=file)
+    parser.add_argument('a_col', type=int, default=1)
     parser.add_argument('b', type=file)
-    args = vars(parser.parse_args())
+    parser.add_argument('b_col', type=int, default=1)
+    args = parser.parse_args()
 
-    data_a = set([
-        x.strip()
+    data_a = [
+        x.split('\t')
         for x in args.a.readlines()
-    ])
+    ]
 
-    data_b = set([
-        x.strip()
+    data_b = [
+        x.split('\t')
         for x in args.b.readlines()
-    ])
+    ]
 
-    for hit in data_a.intersection(data_b):
-        print hit
+    data_a_indexed = {
+        row[args.a_col - 1]: row
+        for row in data_a
+    }
+
+    for row in data_b:
+        row_id = row[args.b_col - 1]
+        if row_id in data_a_indexed:
+            print joinStripRow(data_a_indexed[row_id]), joinStripRow(row)
