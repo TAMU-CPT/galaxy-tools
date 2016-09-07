f606f6cf48aef95f0b61de92cdf9eae01c5e5352
Eleni Mijalis <elenimijalis@gmail.com>
2016-09-07 15:29:59 -0500

    update

--- tools/gff3/gff3_filter.py
+++ tools/gff3/gff3_filter.py
@@ -12,8 +12,11 @@ __maintainer__ = "Eric Rasche"
 __email__ = "esr@tamu.edu"
 
 
-def gff_filter(gff3, filter_list, attribute_field='ID', subfeatures=True):
-    filter_strings = [line.strip() for line in filter_list]
+def gff_filter(gff3, id_list=None, id='', attribute_field='ID', subfeatures=True):
+    if id_list:
+        filter_strings = [line.strip() for line in id_list]
+    else:
+        filter_strings = [x.strip() for x in id.split('__cn__')]
     for rec in GFF.parse(gff3):
         rec.features = feature_lambda(
             rec.features,
@@ -27,7 +30,8 @@ def gff_filter(gff3, filter_list, attribute_field='ID', subfeatures=True):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='extract features from a GFF3 file based on ID/qualifiers')
     parser.add_argument('gff3', type=file, help='GFF3 annotations')
-    parser.add_argument('filter_list', type=file, help='Child GFF3 annotations to rebase against parent')
+    parser.add_argument('--id_list', type=file)
+    parser.add_argument('--id', type=str)
     parser.add_argument('--attribute_field', type=str, help='Column 9 Field to search against', default='ID')
     parser.add_argument('--subfeatures', action='store_true', help='Retain subfeature tree of matched features')
     args = parser.parse_args()
