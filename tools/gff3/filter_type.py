89b2c9761b626c83b0767b49e168af466d3861fa
Eleni Mijalis <elenimijalis@gmail.com>
2016-09-26 12:05:25 -0500

    new filter type

--- tools/gff3/filter_type.py
+++ tools/gff3/filter_type.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python
 import sys
 import argparse
-from BCBio import GFF
-from gff3 import feature_lambda, feature_test_type
+import gffutils
+
+"""
+Output differences from old filter_type (one that uses bcbio-gff):
+    - 'remark' feature is no longer generated
+"""
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('gff3', type=file, help='GFF3 annotations')
+    parser.add_argument('gff3', type=str, help='GFF3 annotations')
     parser.add_argument('types', type=str, nargs='+', help='Feature type to filter on')
     parser.add_argument('--invert', action='store_true')
     args = parser.parse_args()
 
-    for rec in GFF.parse(args.gff3):
-        rec.features = feature_lambda(
-            rec.features,
-            feature_test_type,
-            {'types': args.types},
-            invert=args.invert,
-            subfeatures=False,
-        )
-        GFF.write([rec], sys.stdout)
+    # not sure about merge strategy
+    db = gffutils.create_db(args.gff3, 'test.db', merge_strategy="merge", force=True)
+    db = gffutils.FeatureDB('test.db')
+
+    for d in db.directives:
+        sys.stdout.write('##{0}\n'.format(d))
+    for feature in db.all_features():
+        if (feature.featuretype in args.types) ^ args.invert:
+            sys.stdout.write(str(feature) + '\n')
