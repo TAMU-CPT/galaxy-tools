8d273854ef9c1e7d3f5cd422e3b66c552fb70fc6
Eleni Mijalis <elenimijalis@gmail.com>
2016-10-28 11:49:10 -0500

    guanine script"

--- tools/464/guanine_report.py
+++ tools/464/guanine_report.py
@@ -1,18 +1,36 @@
-#!/usr/bin/env python
+import requests
 import argparse
-import logging
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger(__name__)
+import json
 
+def auth(creds):
+    url = "http://localhost:8000/api-token-auth/"
+    r = requests.post(url, data=json.load(creds));
+    return 'JWT ' + r.json()['token']
 
-def guanine_pass(user_email, test_name):
-    from guanine import GuanineClient
-    g = GuanineClient()
-    g.submit(user_email, test_name, 1.0)
+def post_result(student_id, points_earned, points_possible, token):
+    url = "http://localhost:8000/results/"
+    headers = {'Authorization': token}
+    values = {'student' : student_id,
+              'assessment' : 'd18cfd41-85e2-4e75-8333-22339e05edc2',
+              'points_earned' : points_earned,
+              'points_possible' : points_possible}
+    r = requests.post(url, data=values, headers=headers);
+
+def student_id(email):
+    email = email.replace('@', '%40')
+    url = "http://localhost:8000/students/?email=" + email
+    r = requests.get(url)
+    return r.json()['results'][0]['id']
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='verify against expectations')
-    parser.add_argument('user_email', help='User email')
-    parser.add_argument('test_name', help='Assessment ID')
+    parser = argparse.ArgumentParser(description='post an assessment result')
+    parser.add_argument('creds', type=file, help='json file with username/password')
+    parser.add_argument('student_email', help='email of the student receiving result')
+    parser.add_argument('points_earned', type=int, help='how many points the student earned')
+    parser.add_argument('points_possible', type=int, help='how many points were possible in the assessment')
     args = parser.parse_args()
-    guanine_pass(**vars(args))
+
+    token = auth(args.creds)
+    student_id = student_id(args.student_email)
+    post_result(student_id, args.points_earned, args.points_possible, token)
+
