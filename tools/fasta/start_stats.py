fd9cad744f31bbc28e35c9a23214192291682638
Eric Rasche <rasche.eric@gmail.com>
2016-12-13 20:46:51 +0000

    New fasta only versions of start/stop tab tools

---
+++ tools/fasta/start_stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import argparse
+from Bio import SeqIO
+
+def extract_starts(fasta):
+    codon_usage = {}
+    for record in SeqIO.parse(fasta, 'fasta'):
+        seq = record.seq[0:3]
+        sseq = str(seq)
+        try:
+            codon_usage[sseq] = (codon_usage[sseq][0] + 1, seq)
+        except KeyError:
+            codon_usage[sseq] = (1, seq)
+
+    for (sseq, (count, seq)) in sorted(codon_usage.items()):
+        yield (
+            sseq,
+            codon_usage[sseq][0],
+            seq.translate(table=11)
+        )
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Summarise start codon usage', epilog="")
+    parser.add_argument('fasta', type=argparse.FileType("r"), help='Fasta Genome')
+    args = parser.parse_args()
+
+    print('# DNA\tCodon\tCount')
+    for (key, value, codon) in extract_starts(**vars(args)):
+        print('{}\t{}\t{}'.format(key, codon, value))
---
+++ tools/fasta/stop_stats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import argparse
+from Bio import SeqIO
+
+STOP_CODON_NAMES = {
+    'TAG': 'Amber',
+    'TAA': 'Ochre',
+    'TGA': 'Opal',
+}
+
+
+def extract_stops(fasta):
+    codon_usage = {}
+    for record in SeqIO.parse(fasta, 'fasta'):
+        seq = str(record.seq)[-3:]
+        try:
+            codon_usage[seq] += 1
+        except KeyError:
+            codon_usage[seq] = 1
+
+    for key in sorted(codon_usage):
+        yield (
+            STOP_CODON_NAMES.get(key.upper(), 'None'),
+            key,
+            str(codon_usage[key])
+        )
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Summarise stop codon usage', epilog="")
+    parser.add_argument('fasta', type=argparse.FileType("r"), help='CDS Sequence (DNA + stop (obviously))')
+    args = parser.parse_args()
+    print('# Name\tCodon\tCount')
+    for (name, key, value) in extract_stops(**vars(args)):
+        print('{}\t{}\t{}'.format(name, key, value))
