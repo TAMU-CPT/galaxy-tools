38f1369f434f917df0ff887a0f3f905db6285733
Eric Rasche <rasche.eric@gmail.com>
2016-12-19 19:10:13 +0000

    nicer formatting

--- tools/webapollo/webapollo.py
+++ tools/webapollo/webapollo.py
@@ -861,7 +861,10 @@ def galaxy_list_orgs(trans, *args, **kwargs):
     # And then filter by those which the user has permissions on.
     orgs = accessible_organisms(gx_user, all_orgs)
 
-    return [
-        ("{commonName} ({genus} {species})".format(**org), org['id'], False)
-        for org in orgs
-    ]
+    response = []
+    for org in orgs:
+        if 'genus' in org and org['genus']:
+            response.append(("{commonName} ({genus} {species})".format(**org), org['id'], False))
+        else:
+            response.append(("{commonName}".format(**org), org['id'], False))
+    return response
