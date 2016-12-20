110a030e23e37ea6a1eae0f5ec72bfaafc32f8ed
Eleni Mijalis <elenimijalis@gmail.com>
2016-12-19 21:17:29 -0600

    trying feature lambda

--- tools/gff3/gff3_diff.py
+++ tools/gff3/gff3_diff.py
@@ -3,7 +3,7 @@ import sys
 import logging
 import argparse
 from BCBio import GFF
-from gff3 import feature_lambda, feature_test_qual_value
+from gff3 import feature_lambda, feature_test_type
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
@@ -11,24 +11,29 @@ log = logging.getLogger(__name__)
 def gff3_diff(gff3_1, gff3_2):
     recs1 = {}
     recs2 = {}
-
     for rec1 in GFF.parse(gff3_1):
-        for feature in rec1.features:
-            if feature.location.strand == 1:
-                recs1[feature.location.start] = feature
+        for feat in feature_lambda(rec1.features, feature_test_type, {'type': 'gene'}, subfeatures=True):
+            if feat.location.strand == 1:
+                recs1[feat.location.start] = feat
             else:
-                recs1[feature.location.end] = feature
+                recs1[feat.location.end] = feat
 
     for rec2 in GFF.parse(gff3_2):
-        for feature in rec2.features:
-            if feature.location.strand == 1:
-                recs2[feature.location.start] = feature
+        for feat in feature_lambda(rec2.features, feature_test_type, {'type': 'gene'}, subfeatures=True):
+            if feat.location.strand == 1:
+                recs2[feat.location.start] = feat
             else:
-                recs2[feature.location.end] = feature
+                recs2[feat.location.end] = feat
 
 
+    print len(recs1)
+    print len(recs2)
     for i in recs1:
-        print i, recs2[i]
+        a = recs1[i]
+        try:
+            b = recs2[i]
+        except:
+            print recs1[i]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Reports differences between two gff3 files')
