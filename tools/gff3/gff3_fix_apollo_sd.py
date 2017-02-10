adc99f48990dac6fbe671564a78e65e203baf274
Eric Rasche <rasche.eric@gmail.com>
2017-02-10 19:40:33 +0000

    new tool

---
+++ tools/gff3/gff3_fix_apollo_sd.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys
+import logging
+import argparse
+from BCBio import GFF
+from gff3 import feature_lambda
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+
+def test_true(feature, **kwargs):
+    return True
+
+
+def gff_filter(gff3):
+    for rec in GFF.parse(gff3):
+        for feature in feature_lambda(
+                rec.features, test_true, {}, subfeatures=True):
+            if feature.type == 'exon' and len(feature) < 20:
+                feature.type = 'Shine_Dalgarno_sequence'
+
+        rec.annotations = {}
+        GFF.write([rec], sys.stdout)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='original annotation set')
+    parser.add_argument('gff3', type=argparse.FileType("r"), help='GFF3 annotations')
+    args = parser.parse_args()
+    gff_filter(**vars(args))
