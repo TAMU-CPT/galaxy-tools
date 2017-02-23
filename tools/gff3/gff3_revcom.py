8011ad8a8bb804e908404eb23054220c911b8f0d
Eric Rasche <rasche.eric@gmail.com>
2017-02-23 00:29:51 +0000

    fancier revcom

--- tools/gff3/gff3_revcom.py
+++ tools/gff3/gff3_revcom.py
@@ -2,20 +2,34 @@
 import sys
 import logging
 import argparse
+from Bio import SeqIO
 from BCBio import GFF
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-def gff_filter(gff3):
-    for rec in GFF.parse(gff3):
+def gff_filter(gff3, fasta=None):
+    if fasta:
+        seq_dict = SeqIO.to_dict(SeqIO.parse(fasta, "fasta"))
+        it = GFF.parse(gff3, base_dict=seq_dict)
+    else:
+        it = GFF.parse(gff3)
+
+    for rec in it:
         rec.annotations = {}
-        GFF.write([rec.reverse_complement(id=True, name=True, description=True, features=True, annotations=True, letter_annotations=True, dbxrefs=True)], sys.stdout)
+        rec = rec.reverse_complement(id=True, name=True, description=True, features=True, annotations=True, letter_annotations=True, dbxrefs=True)
+        yield rec
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='reverse and complement as set of GFF3 annotations')
     parser.add_argument('gff3', type=argparse.FileType("r"), help='GFF3 annotations')
+    parser.add_argument('--fasta', type=argparse.FileType("r"), help='Optional fasta file')
+    parser.add_argument('--fasta_output', type=argparse.FileType("w"), help='Optional fasta file output', default='reopened.fasta')
     args = parser.parse_args()
 
-    gff_filter(**vars(args))
+    for rec in gff_filter(**vars(args)):
+        GFF.write([rec], sys.stdout)
+
+        if args.fasta:
+            SeqIO.write([rec], args.fasta_output, 'fasta')
