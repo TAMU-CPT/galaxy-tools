be4e39a7d20456f3b806274c36b05bfea957c9e1
Eric Rasche <rasche.eric@gmail.com>
2017-02-27 21:42:45 +0000

    Changes from more cacao work

--- tools/cacao/add_org.py
+++ tools/cacao/add_org.py
@@ -12,13 +12,24 @@ def auth(creds, url):
     return 'JWT ' + r.json()['token']
 
 
+def get(token, url):
+    print('>', url)
+    q = requests.get(
+        url,
+        headers={'Authorization': token}
+    ).json()
+    print('<', q)
+    return q
+
+
 def post(token, url, data):
+    print('>', data)
     q = requests.post(
         url,
         data=data,
         headers={'Authorization': token}
     ).json()
-    print(q)
+    print('<', q)
     return q
 
 
@@ -27,23 +38,31 @@ def main():
     parser.add_argument('url', help='Backend URL. Include trailing slash.')
     parser.add_argument('creds', type=argparse.FileType("r"), help='json file with username/password')
 
-    parser.add_argument('--organism_file', type=argparse.FileType('r'))
     parser.add_argument('--taxon', type=str, default='NA')
     parser.add_argument('--ebi_id', type=str, default='NA')
     parser.add_argument('gff3', type=argparse.FileType('r'))
     parser.add_argument('fasta', type=argparse.FileType('r'))
+    parser.add_argument('organism_file', type=argparse.FileType('r'))
 
     args = parser.parse_args()
 
     token = auth(args.creds, args.url)
 
-    organism_name = json.load(args.organism_file)[0]['common_name']
+    organism_name = json.load(args.organism_file)[0]
+    if 'commonName' in organism_name:
+        organism_name = organism_name['commonName']
+    elif 'common_name' in organism_name:
+        organism_name = organism_name['common_name']
+    else:
+        raise Exception("Bad input organism data")
 
     organism = post(token, args.url + 'organisms/', dict(
         common_name=organism_name,
         taxon=args.taxon,
         ebi_id=args.ebi_id
     ))
+    if organism.get('common_name', [None])[0] == 'organism with this common name already exists.':
+        organism = get(token, args.url + 'organisms/?common_name=' + organism_name)['results'][0]
 
     refseqs = {}
     for record in SeqIO.parse(args.fasta, "fasta"):
@@ -52,6 +71,9 @@ def main():
             length=len(record.seq),
             organism=organism['id'],
         ))
+        if refseq.get('non_field_errors', [None])[0] == 'The fields name, organism must make a unique set.':
+            refseq = get(token, args.url + 'refseq/?name=%s&%s' % (record.id, organism['id']))['results'][0]
+
         refseqs[record.id] = refseq
 
     for rec in GFF.parse(args.gff3):
@@ -62,6 +84,7 @@ def main():
                 continue
 
             post(token, args.url + 'genes/', {
+                "id": feat.id,
                 "start": int(feat.location.start),
                 "end": int(feat.location.end),
                 "strand": int(feat.location.strand),
