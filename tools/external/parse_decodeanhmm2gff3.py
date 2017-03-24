94a1338f80826e4f47eada3c861c0a8a4a758fca
Eric Rasche <rasche.eric@gmail.com>
2017-03-24 18:57:50 +0000

    Add wig support to TMHMM

--- tools/external/parse_decodeanhmm2gff3.py
+++ tools/external/parse_decodeanhmm2gff3.py
@@ -1,28 +1,40 @@
 #!/usr/bin/env python
 import sys
 import uuid
+import argparse
 import logging
 from BCBio import GFF
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.SeqFeature import SeqFeature, FeatureLocation
 from Bio.Alphabet import IUPAC
+from bigwig import bigwig_add_header, bigwig_store
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-def convert(data=None):
+def convert(data=None, bw_i=None, bw_o=None, bw_m=None):
     record = None
+    i_data = []
+    o_data = []
+    m_data = []
     length = None
     count = 1
 
     if data is None:
         data = sys.stdin
 
-    for line in data:
+    for idx, line in enumerate(data):
         if line.startswith('>'):
             if record is not None and len(record.features) > 0:
-                yield [record]
+                bigwig_store(bw_i, record.id, i_data)
+                bigwig_store(bw_o, record.id, o_data)
+                bigwig_store(bw_m, record.id, m_data)
+                yield record
+                # Reset
+                i_data = []
+                o_data = []
+                m_data = []
 
             header = line.split(' ')[0].strip()[1:]
             record = SeqRecord(
@@ -30,10 +42,10 @@ def convert(data=None):
                 id=header
             )
 
-        if line.startswith('%len'):
+        elif line.startswith('%len'):
             length = int(line[5:].strip())
 
-        if line.startswith('%pred'):
+        elif line.startswith('%pred'):
             pred = line.strip()
             pred = pred[pred.index(': ') + 2:]
             # %pred NB(0): i 1 8, M 9 28, o 29 44
@@ -97,9 +109,47 @@ def convert(data=None):
                 feature.sub_features.append(sub_feat)
 
             record.features.append(feature)
-    yield [record]
+        else:
+            if record:
+                parts = line.split()
+                if len(parts) != 5:
+                    continue
+
+                if parts[0] == '#':
+                    continue
+
+                # ['#', 'i', 'O', 'o', 'M']
+                # in, out, ?, membrane potential
+                i_data.append(float(parts[1]))
+                o_data.append(float(parts[3]))
+                m_data.append(float(parts[4]))
+
+                # print record.id, parts
+                # print record.id, line,
+    bigwig_store(bw_i, record.id, i_data)
+    bigwig_store(bw_o, record.id, o_data)
+    bigwig_store(bw_m, record.id, m_data)
+    yield record
 
 
 if __name__ == '__main__':
-    for record in convert():
-        GFF.write(record, sys.stdout)
+    parser = argparse.ArgumentParser(description='Process TMHMM outputs in GFF3, BigWig')
+    parser.add_argument('--bw_i', default='tmhmm_i.wig')
+    parser.add_argument('--bw_o', default='tmhmm_o.wig')
+    parser.add_argument('--bw_m', default='tmhmm_m.wig')
+    args = parser.parse_args()
+
+    bw_i = open(args.bw_i, 'w')
+    bw_o = open(args.bw_o, 'w')
+    bw_m = open(args.bw_m, 'w')
+
+    bigwig_add_header(bw_i, 'i', name='TMHMM')
+    bigwig_add_header(bw_o, 'o', name='TMHMM')
+    bigwig_add_header(bw_m, 'm', name='TMHMM')
+
+    for sequence in convert(None, bw_i, bw_o, bw_m):
+        GFF.write([sequence], sys.stdout)
+
+    bw_i.close()
+    bw_o.close()
+    bw_m.close()
