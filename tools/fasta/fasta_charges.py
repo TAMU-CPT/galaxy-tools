55be66eecdbb412bc7f906907713ef33980c0b0c
Eric Rasche <rasche.eric@gmail.com>
2017-04-03 14:47:24 +0000

    Fixes #144

--- tools/fasta/fasta_charges.py
+++ tools/fasta/fasta_charges.py
@@ -1,40 +1,16 @@
 #!/usr/bin/env python
 import argparse
 from Bio import SeqIO
-import sys
-
 import logging
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(name='charges')
 
-COLOUR_SCHEMES = {
-    'default': {
-        'polar': {
-            # must be uppercase
-            'match': 'HSQTNCY',
-            'fg': 'black',
-            'bg': 'white'
-        },
-        'hydrophobic': {
-            'match': 'AVILMPFWG',
-            'fg': 'black',
-            'bg': '#999',
-        },
-        'charged': {
-            'match': 'ERDK',
-            'fg': 'white',
-            'bg': 'black',
-        }
-    }
-}
 HTML_HEADER = '<html><head><title>Charges Report</title></head><body>'
 HTML_FOOTER = '</body></html>'
 
 
-def charges_html(fasta, cs='default', width=120):
-    if cs not in COLOUR_SCHEMES:
-        log.error("Colour scheme not known to script")
-        sys.exit(1)
+def charges_html(fasta, aa, fgColor, bgColor, width=120):
+    colour_scheme = zip([x.upper() for x in aa], fgColor, bgColor)
 
     # CSS and header styling
     css = """<style type="text/css">
@@ -43,14 +19,14 @@ def charges_html(fasta, cs='default', width=120):
     pre { font-size:1.3em }
     """
     info = '<h1>Charges</h1><h3>Legend</h3><ul class="list">'
-    for group in COLOUR_SCHEMES[cs]:
-        css += '.%(match)s{ background: %(bg)s; color: %(fg)s}\n' % COLOUR_SCHEMES[cs][group]
-        info += '<li><span class="%(match)s" style="padding:5px">%(match)s</span></li>\n' % COLOUR_SCHEMES[cs][group]
+    for group in colour_scheme:
+        css += '.%s{ background: %s; color: %s}\n' % group
+        info += '<li><span class="%s" style="padding:5px">%s</span></li>\n' % (group[0], group[0])
     css += '</style>'
     info += '</ul>'
 
     # Pre-calculate, so we can use for testing 'in'
-    match_list = [COLOUR_SCHEMES[cs][x]['match'] for x in COLOUR_SCHEMES[cs]]
+    match_list = [group[0] for group in colour_scheme]
 
     page = ''
     # Parse sequences from fasta file
@@ -79,7 +55,7 @@ def charges_html(fasta, cs='default', width=120):
 
                 # Replace with <span>
                 for m in match_list:
-                    if line_residues[char] in m:
+                    if line_residues[char].upper() in m:
                         line_residues[char] = '<span class="%s">%s</span>' % (m, line_residues[char])
 
             page += ''.join(line_charges) + '\n'
@@ -93,8 +69,10 @@ def charges_html(fasta, cs='default', width=120):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Top related genomes')
     parser.add_argument('fasta', type=argparse.FileType("r"), help='Fasta protein file')
-    parser.add_argument('--cs', help='Colour scheme', default='default')
     parser.add_argument('--width', type=int, help='Plot width', default=120)
+    parser.add_argument('--aa', nargs='+')
+    parser.add_argument('--fgColor', nargs='+')
+    parser.add_argument('--bgColor', nargs='+')
     args = parser.parse_args()
 
-    print charges_html(**vars(args))
+    print(charges_html(**vars(args)))
