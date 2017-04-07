d5a7daa9809a8cb772509a2806925f94a364a6eb
Eric Rasche <rasche.eric@gmail.com>
2017-04-07 18:34:59 +0000

    Script to fix tRNA model

---
+++ tools/external/fix-trnascan-gff3.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import sys
+import logging
+import argparse
+from BCBio import GFF
+from Bio.SeqFeature import SeqFeature
+from gff3 import feature_lambda, feature_test_type
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
+
+def fixed_feature(rec):
+    for idx, feature in enumerate(feature_lambda(
+        rec.features,
+        feature_test_type,
+        {'type': 'tRNA'},
+        subfeatures=True
+    )):
+        fid = 'tRNA-%03d' % (1 + idx)
+        gene = SeqFeature(
+            location=feature.location,
+            type='gene',
+            qualifiers={
+                'ID': [fid + '.gene'],
+                'source': ['aragorn'],
+            }
+        )
+        # Below that we have an mRNA
+        exon = SeqFeature(
+            location=feature.location,
+            type='exon',
+            qualifiers={
+                'source': ['aragorn'],
+                'ID': ['%s.exon' % fid]
+            }
+        )
+        feature.qualifiers['ID'] = [fid]
+
+        gene.sub_features = [exon, feature]
+        yield gene
+
+
+def gff_filter(gff3):
+    for rec in GFF.parse(gff3):
+        rec.features = sorted(list(fixed_feature(rec)), key=lambda x: x.location.start)
+        rec.annotations = {}
+        GFF.write([rec], sys.stdout)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='add parent gene features to CDSs')
+    parser.add_argument('gff3', type=argparse.FileType("r"), help='GFF3 annotations')
+    args = parser.parse_args()
+    gff_filter(**vars(args))
