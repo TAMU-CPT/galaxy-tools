803620a12152ae355abe127e73229523129a4baf
Eric Rasche <rasche.eric@gmail.com>
2017-04-10 18:29:09 +0000

    log file, fix bug

--- tools/comparative/cdhit-reformat.py
+++ tools/comparative/cdhit-reformat.py
@@ -9,10 +9,17 @@ logging.basicConfig(level=logging.INFO)
 def extract_clust(cdhit, min_size):
     cluster = []
     cluster_id = None
+    output = 0
+    skipped = 0
+
     for line in cdhit:
         if line.startswith('>'):
             if len(cluster) >= min_size:
                 yield cluster, cluster_id
+                output += 1
+            else:
+                if cluster_id:
+                    skipped += 1
             cluster = []
             cluster_id = line.strip().split(' ')[1]
         elif cluster_id is not None:
@@ -25,6 +32,10 @@ def extract_clust(cdhit, min_size):
 
     if len(cluster) >= min_size:
         yield cluster, cluster_id
+        output += 0
+    else:
+        skipped += 1
+    print "Filtered %0.3f%% of clusters" % (100 * float(output) / (output + skipped))
 
 
 def cdhit_process(cdhit, fasta, output_dir, min_size=1):
