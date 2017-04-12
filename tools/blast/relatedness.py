e8fb660fa939a84c5355d78f1dfc61928fe9fa16
Eric Rasche <rasche.eric@gmail.com>
2017-04-12 21:43:03 +0000

    Unified tool

--- tools/blast/relatedness.py
+++ tools/blast/relatedness.py
@@ -6,10 +6,6 @@ import json
 import logging
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger()
-with open('db.json', 'r') as handle:
-    phage_names = json.load(handle)
-
-phageNameLookup = {k['desc'].rstrip('.'): k['id'] for k in phage_names}
 
 
 def parse_blast(blast):
@@ -29,7 +25,7 @@ def filter_dice(blast, threshold=0.5):
             yield data
 
 
-def split_identifiers(ident):
+def split_identifiers_nucl(ident):
     if '<>' in ident:
         idents = ident.split('<>')
     else:
@@ -37,7 +33,20 @@ def split_identifiers(ident):
     return idents
 
 
-def important_only(blast):
+def split_identifiers_prot(ident):
+    if '<>' in ident:
+        idents = ident.split('<>')
+    else:
+        idents = [ident]
+    return [
+        x[x.index('[') + 1:x.rindex(']')]
+        for x in idents
+        # MULTISPECIES: recombination-associated protein RdgC [Enterobacteriaceae]<>RecName: Full=Recombination-associated protein RdgC<>putative exonuclease, RdgC [Enterobacter sp. 638]
+        if '[' in x and ']' in x
+    ]
+
+
+def important_only(blast, split_identifiers):
     # Connect to kyoto db
     for data in blast:
         yield [
@@ -80,7 +89,7 @@ def deform_scores(blast):
             ]
 
 
-def filter_phage(blast):
+def filter_phage(blast, phageNameLookup):
     for data in blast:
         if data[1] in phageNameLookup:
             yield [
@@ -104,15 +113,25 @@ def scoreMap(blast):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Top related genomes')
     parser.add_argument('blast', type=argparse.FileType("r"), help='Blast 25 Column Results')
+    parser.add_argument('phagedb', type=argparse.FileType("r"))
+    parser.add_argument('--protein', action='store_true')
 
     args = parser.parse_args()
+
+    phageDb = json.load(args.phagedb)
+    if args.protein:
+        splitId = split_identifiers_prot
+        phageNameLookup = {k['source'].rstrip('.'): k['id'] for k in phageDb}
+    else:
+        splitId = split_identifiers_nucl
+        phageNameLookup = {k['desc'].rstrip('.'): k['id'] for k in phageDb}
 
     data = parse_blast(args.blast)
     data = with_dice(data)
     data = filter_dice(data, threshold=0.3)
-    data = important_only(data)
+    data = important_only(data, splitId)
     data = deform_scores(data)
-    data = filter_phage(data)
+    data = filter_phage(data, phageNameLookup)
 
     scores = scoreMap(data)
     sys.stdout.write('# ID\tName\tScore\n')
