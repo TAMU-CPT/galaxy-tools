a0b4ce44b889dbf6231ca8b7c942fb070064f1a3
Eleni Mijalis <elenimijalis@gmail.com>
2017-04-25 11:05:55 -0500

    fix

--- tools/gbk/split_gbks.py
+++ tools/gbk/split_gbks.py
@@ -2,12 +2,14 @@
 import argparse
 from Bio import SeqIO
 from zipfile import ZipFile
+import os
 
 
 def spl(genbank):
+    os.makedirs('gbk_out')
     with ZipFile('out.zip', 'w') as zipfile:
         for record in SeqIO.parse(genbank, 'genbank'):
-            filename = record.id + '.gbk'
+            filename = os.path.join('gbk_out', record.id + '.gbk')
             with open(filename, 'w') as handle:
                 SeqIO.write([record], handle, 'genbank')
             zipfile.write(filename)
