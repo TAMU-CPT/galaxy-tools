ef7d1ed34197042354a780f65d07a8a843fdbd00
Eric Rasche <esr@tamu.edu>
2017-06-05 12:34:38 -0500

    Updated downloader

--- tools/sequencing/BaseSpaceRunDownloader_v2.py
+++ tools/sequencing/BaseSpaceRunDownloader_v2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from urllib2 import Request, urlopen, URLError
 from os.path import expanduser
 import re
@@ -24,7 +25,7 @@ ACCESS_TOKEN = '?access_token=%s' % AccessToken
 
 def restrequest(rawrequest):
     rawrequest = API_BASE + rawrequest + ACCESS_TOKEN
-    log.debug('Req: ' + rawrequest.replace(AccessToken, '*' * len(AccessToken)))
+    log.info('Req: ' + rawrequest.replace(AccessToken, '*' * len(AccessToken)))
 
     req_url_hash = hashlib.md5(rawrequest).hexdigest()
     cache_path_basedir = os.path.join(expanduser("~"), '.cache', 'basespace')
@@ -44,8 +45,8 @@ def restrequest(rawrequest):
         response = urlopen(request)
         json_string = response.read()
         json_obj = json.loads(json_string)
-    except URLError, e:
-        print 'Got an error code:', e
+    except URLError as e:
+        print('Got an error code:', e)
         sys.exit()
 
     with open(cache_path, 'w') as handle:
@@ -54,37 +55,38 @@ def restrequest(rawrequest):
     return json_obj
 
 
-json_obj = restrequest('v1pre3/runs/%s/samples' % RunID)
+json_obj = restrequest('v1pre3/runs/%s' % RunID)
 
-for sample in json_obj['Response']['Items']:
-    sample_json_obj = restrequest(sample['Href'])
+for item in json_obj['Response']['Properties']['Items']:
+    for item2 in item['Items']:
+        sample_json_obj = restrequest(item2['Href'])
 
-    files_obj = restrequest(sample_json_obj['Response']['HrefFiles'])
+        files_obj = restrequest(sample_json_obj['Response']['HrefFiles'])
 
-    for file in files_obj['Response']['Items']:
-        potential_filename = file['Name']
-        safe_filename = re.sub('[^A-Za-z0-9._-]', '', potential_filename)
-        fastq_url = file['HrefContent']
+        for file in files_obj['Response']['Items']:
+            potential_filename = file['Name']
+            safe_filename = re.sub('[^A-Za-z0-9._-]', '', potential_filename)
+            fastq_url = file['HrefContent']
 
-        response = urlopen(API_BASE + fastq_url + ACCESS_TOKEN, timeout=10)
+            response = urlopen(API_BASE + fastq_url + ACCESS_TOKEN, timeout=1200)
 
-        content_length = int(response.info().getheader('Content-Length'))
-        expected_chunks = content_length / CHUNK_SIZE
+            content_length = int(response.info().getheader('Content-Length'))
+            expected_chunks = content_length / CHUNK_SIZE
 
-        if os.path.exists(safe_filename) and os.path.getsize(safe_filename) == content_length:
-            log.info("%s already downloaded and is correct size" % safe_filename)
-            continue
-        elif os.path.exists(safe_filename):
-            log.warning("%s exists but may be a partial download / different file" % safe_filename)
-            sys.exit()
+            if os.path.exists(safe_filename) and os.path.getsize(safe_filename) == content_length:
+                log.info("%s already downloaded and is correct size" % safe_filename)
+                continue
+            elif os.path.exists(safe_filename):
+                log.warning("%s exists but may be a partial download / different file" % safe_filename)
+                sys.exit()
 
-        with open(safe_filename, 'wb') as handle:
-            i = 0
-            while True:
-                i += 1
-                chunk = response.read(CHUNK_SIZE)
-                if not chunk:
-                    break
-                handle.write(chunk)
+            with open(safe_filename, 'wb') as handle:
+                i = 0
+                while True:
+                    i += 1
+                    chunk = response.read(CHUNK_SIZE)
+                    if not chunk:
+                        break
+                    handle.write(chunk)
 
-print json.dumps(json_obj)
+print(json.dumps(json_obj))
