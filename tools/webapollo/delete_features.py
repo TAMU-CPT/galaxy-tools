1f2993637fa23ce393b622c7c66037ba5d3312cd
E Rasche <hxr@hx42.org>
2017-06-21 16:37:02 +0000

    add simple feature deletion script

--- tools/webapollo/delete_features.py
+++ tools/webapollo/delete_features.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sample script to delete all features from an organism')
     WAAuth(parser)
     parser.add_argument('email', help='User Email')
+    parser.add_argument('--type', help='Feature type filter')
     OrgOrGuess(parser)
 
     args = parser.parse_args()
@@ -37,6 +38,22 @@ if __name__ == '__main__':
         features = wa.annotations.getFeatures()
         # For each feature in the features
         for feature in sorted(features['features'], key=lambda x: random.random()):
+            if args.type:
+                if args.type == 'tRNA':
+                    if feature['type']['name'] != 'tRNA':
+                        continue
+
+                elif args.type == 'terminator':
+                    if feature['type']['name'] != 'terminator':
+                        continue
+
+                elif args.type == 'mRNA':
+                    if feature['type']['name'] != 'mRNA':
+                        continue
+
+                else:
+                    raise Exception("Unknown type")
+
             # We see that deleteFeatures wants a uniqueName, and so we pass
             # is the uniquename field in the feature.
             def fn():
