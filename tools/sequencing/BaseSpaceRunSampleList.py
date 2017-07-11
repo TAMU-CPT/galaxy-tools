bba9db5734d42bef7764aa3825b62de71680d37d
Cory Maughmer <cory.maughmer@gmail.com>
2017-07-11 14:21:36 -0500

    Forgot how python import works

--- tools/sequencing/BaseSpaceRunSampleList.py
+++ tools/sequencing/BaseSpaceRunSampleList.py
@@ -6,7 +6,6 @@ import json
 import sys
 import os
 import logging
-from BaseSpaceRunDownloader_v2 import restquery
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger()
 
@@ -48,6 +47,42 @@ def restrequest(rawrequest):
 
     return json_obj
 
+
+def restquery(rawrequest, parameters):
+    # Parameters is a dict example: {'Limit': 50, 'SortDir': 'Asc'}
+    # can be any of https://developer.basespace.illumina.com/docs/content/documentation/rest-api/api-reference#ResourceCollectionRequests
+    rawrequest = API_BASE + rawrequest + ACCESS_TOKEN
+    for query, value in parameters.items():
+        rawrequest+= '&' + query + '=' + str(value)
+
+    log.info('Req: ' + rawrequest.replace(AccessToken, '*' * len(AccessToken)))
+
+    req_url_hash = hashlib.md5(rawrequest).hexdigest()
+    cache_path_basedir = os.path.join(expanduser("~"), '.cache', 'basespace')
+    if not os.path.exists(cache_path_basedir):
+        os.makedirs(cache_path_basedir)
+
+    cache_path = os.path.join(cache_path_basedir, req_url_hash)
+    if os.path.exists(cache_path):
+        with open(cache_path, 'r') as handle:
+            json_obj = json.load(handle)
+            return json_obj
+
+    log.debug("Cache miss")
+    request = Request(rawrequest)
+
+    try:
+        response = urlopen(request)
+        json_string = response.read()
+        json_obj = json.loads(json_string)
+    except URLError as e:
+        print('Got an error code:', e)
+        sys.exit()
+
+    with open(cache_path, 'w') as handle:
+        json.dump(json_obj, handle)
+
+    return json_obj
 
 json_obj = restquery('v1pre3/runs/%s/properties/Output.Samples/items' % RUN_ID, {'Limit': 1024})
 print(json.dumps(json_obj))
