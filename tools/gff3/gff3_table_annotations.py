05d33096169e93b4c83acaa1e01244e81c0819fd
BeaverThing <talkingbeaverthing@gmail.com>
2019-03-28 11:20:34 -0500

    Table_Annotations RC1

--- tools/gff3/gff3_table_annotations.py
+++ tools/gff3/gff3_table_annotations.py
@@ -7,36 +7,34 @@ from Bio.Seq import Seq
 from gff3 import feature_lambda, feature_test_true
 import csv
 import argparse
-import os #REMOVE BEFORE PUSH
 
-
-def table_annotations(gff3In, tabularIn, fastaIn, out_gff3):
-
-    if os.path.exists(os.getcwd()+"/test-data/changelog.txt"):
-      os.remove(os.getcwd()+"/test-data/changelog.txt")
-    if os.path.exists(os.getcwd()+"/test-data/snoke-edit.gff3"):
-      os.remove(os.getcwd()+"/test-data/snoke-edit.gff3")
+def table_annotations(gff3In, tabularIn, fastaIn, out_gff3, out_changelog):
 
     # CSV parse tabular
     idDict = csv.DictReader(tabularIn, delimiter = '\t', fieldnames = ['OrgID', 'ID', 'Name', 'BoundS', 'BoundE', 'Strand', 'Note'])
 
-    out_handle = open("test-data/snoke-edit.gff3", "a")
     # BioPython parse GFF
-    
-    #seq_dict = SeqIO.to_dict(SeqIO.parse(fastaIn, "fasta"))
     recG = list(GFF.parse(gff3In, SeqIO.to_dict(SeqIO.parse(fastaIn, "fasta"))))[0]
 
     # Get a changelog ready
-    changelog = open("test-data/changelog.txt", "a")
+    out_changelog.write("ID\tChanges\tStatus\n")
 
     anyChange = False
 
     for row in idDict:
+      if row["ID"] == "ID":
+        continue # Skip header
       Found = False;
 
       for i in recG.features:              
         if row["ID"] == i.id:
-       
+          
+          strandC = False
+          startC = False
+          endC = False
+          nameC = False
+          noteC = False
+          
           if row["Strand"] == '+':
             row["Strand"] = +1
           else:
@@ -44,49 +42,80 @@ def table_annotations(gff3In, tabularIn, fastaIn, out_gff3):
 
           Found = True
           Attr = False
-   
-          if i.qualifiers["Name"] != row["Name"]:
-            i.qualifiers["Name"] = row["Name"]  
-            Attr = True
-         
-          if i.location.start != int(row["BoundS"]) or i.location.end != int(row["BoundE"]) or i.strand != row["Strand"]:
-            i.location = FeatureLocation(int(row["BoundS"]), int(row["BoundE"]), strand = row["Strand"] )
-            Attr = True
 
-          if "Note" in i.qualifiers and row["Note"] != "": # Set to not override with blank notes currently
-            row["Note"] = (row["Note"]).split(',')
+          if (row["Name"] != i.qualifiers["Name"][0]):
+            i.qualifiers["Name"][0] = row["Name"]  
+            nameC = True
+         
+          if i.location.start != int(row["BoundS"]):
+            startC = True
+          
+          if i.location.end != int(row["BoundE"]):
+            endC = True
+ 
+          if i.strand != row["Strand"]:
+            strandC = True
+
+          if startC or endC or strandC: # Location object needs to be rebuilt, can't individually set start/end
+            i.location = FeatureLocation(int(row["BoundS"]), int(row["BoundE"]), strand = row["Strand"] )
+
+          if ("Note" in i.qualifiers) and row["Note"] != "": # Set to not override with blank notes currently
+            row["Note"] = (row["Note"]).split(',') # Turn note into a list
             if i.qualifiers["Note"] != row["Note"]:
               i.qualifiers["Note"] = row["Note"]
-              Attr = True
+              noteC = True
+          elif row["Note"] != "":
+            row["Note"] = (row["Note"]).split(',')
+            i.qualifiers["Note"] = row["Note"]
+            noteC = True
           
-
-          # On success, write out replaced attributes and success
-          if Found and Attr:
-            changelog.write(("Successfully overrode %s with \n" % i.id) + str(row) + '\n')
-            anyChange = True
-
-          # On fail, write out table line and why
-          elif Found:
+          changeList = ""
+          if nameC:
+            changeList += "Name"
+          if startC:
+            if changeList != "":
+              changeList += ", "
+            changeList += "Start"            
+          if endC:
+            if changeList != "":
+              changeList += ", "
+            changeList += "End" 
+          if strandC:
+            if changeList != "":
+              changeList += ", "
+            changeList += "Strand" 
+          if noteC:
+            if changeList != "":
+              changeList += ", "
+            changeList += "Notes" 
+          
+          if changeList != "": # On success, write out replaced attributes and success
+            out_changelog.write("%s\t%s\tSuccess\n" % (i.id, changeList))
+            anyChange = True          
+          else: # On fail, write out table line and why
             # No changes detected
-            changelog.write(("Did not override %d : No changes from table.\n Table line was " % i.id) + str(row) + '\n') 
+            out_changelog.write("%s\tNone\tNo Change\n" % i.id) 
       
       if Found == False:
         # No such ID
-        changelog.write("Did not override %s : No such feature found.\n" % row["ID"])
+        out_changelog.write("%s\tNone\tID not Found\n" % row["ID"])
 
     if anyChange:
       #out_handle = open("test-data/snoke-edit.gff3", "a")
+      recG.features = [x for x in recG.features if x.type != 'remark']
       GFF.write([recG], out_gff3)
     else:
-      changelog.write("No changes made to source GFF3 file.\n")
+      out_changelog.write("GFF3\tNone\tGFF3 already equals Table\n")
 
-    changelog.close()
+    out_changelog.close()
+    out_gff3.close()
         
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Update GFF3 input from given tabular file')
     parser.add_argument('gff3In', type=argparse.FileType("r"), help='GFF3 source file')
     parser.add_argument('tabularIn', type=argparse.FileType("r"), help='Tabular file to fetch edits')
-    parser.add_argument('fastaIn', type=argparse.FileType("r"), help='Tabular file to fetch edits')
+    parser.add_argument('fastaIn', type=argparse.FileType("r"), help='Fasta file to correctly construct the GFF3')
     parser.add_argument('--out_gff3', type=argparse.FileType("w"), help='Output gff3', default='out.gff3')
+    parser.add_argument('--out_changelog', type=argparse.FileType("w"), help='Output changelog', default='out.tsv')
     args = parser.parse_args()
     table_annotations(**vars(args))
