e2c482c2a79bd3a967639b6dd3867aee8a069fbe
Anthony C <anthonyc@tamu.edu>
2019-05-10 16:48:35 -0500

    Update to revcom to accept multiple files and .txt files

--- tools/fasta/fasta_revcom.py
+++ tools/fasta/fasta_revcom.py
@@ -2,6 +2,8 @@
 import argparse
 import sys
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
 
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -18,6 +20,19 @@ if __name__ == '__main__':
     )
 
     args = parser.parse_args()
+
+    i = 1
+
     for fa_file in args.fa_files:
-        for record in SeqIO.parse(fa_file, 'fasta'):
-            SeqIO.write(record.reverse_complement(**kwargs), sys.stdout, 'fasta')
+        if '.fa' in fa_file.name.lower(): # Change to endswith and encompass other fasta formats 
+          for record in SeqIO.parse(fa_file, 'fasta'):
+              SeqIO.write(record.reverse_complement(**kwargs), sys.stdout, 'fasta')
+        elif '.txt' in fa_file.name.lower():
+          name = "Txt_File_Input_" + str(i)
+          i += 1
+          tempSeq = Seq("") 
+          for line in fa_file.readlines():
+            tempSeq = tempSeq + line.strip('\n')#.add(Seq(line.strip('\n')))
+          tempRec = SeqRecord(tempSeq, name, name, 'Reverse complement from raw .txt input.')
+          SeqIO.write(tempRec.reverse_complement(**kwargs), sys.stdout, 'fasta')
+            
