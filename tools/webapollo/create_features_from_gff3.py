3b060218dbef980a05efeeae0f92518c71f1b9d7
Anthony C <anthonyc@tamu.edu>
2019-07-16 16:01:31 -0500

    Fix to allow non-CDS subfeatures to be added from GFF3->Apollo

--- tools/webapollo/create_features_from_gff3.py
+++ tools/webapollo/create_features_from_gff3.py
@@ -122,7 +122,7 @@ if __name__ == '__main__':
                     # to set the CDS location correctly (apollo currently screwing
                     # this up (2.0.6))
                     CDS = featureData[0]['children'][0]['children']
-                    CDS = [x for x in CDS if x['type']['name'] == 'CDS'][0]['location']
+                    CDS = [x for x in CDS if x['type']['name'] in ['CDS', 'RBS', 'exon']][0]['location']
                     # Create the new feature
                     newfeature = wa.annotations.addFeature(featureData, trustme=True)
                     # Extract the UUIDs that apollo returns to us
