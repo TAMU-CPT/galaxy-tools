a5adcaa5ebbaa4876276e92332844f522f3229af
Anthony C <anthonyc@tamu.edu>
2019-10-09 10:58:54 -0500

    Attempted workaround for Echo permissions error

--- tools/genemark/KeyGen.py
+++ tools/genemark/KeyGen.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
-f = open(".gm_key_64","w+")
-f.write("ACTAAGGTACGTACAGTCCGATGTTTTTTTTTTTTTTTTCCGATCGGCCATCGTGTAGGACATTCCAGCTTGAAGTACGT\n521146385")
-f.close()
+import argparse
+
+def keyGen(fasta_file):
+  f = open(fasta_file,"w+")
+  f.write("ACTAAGGTACGTACAGTCCGATGTTTTTTTTTTTTTTTTCCGATCGGCCATCGTGTAGGACATTCCAGCTTGAAGTACGT\n521146385")
+  f.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Workaround for making GeneMark key')
+    parser.add_argument('fasta_file', type=str, help='Fasta file')
+    args = parser.parse_args()
+
+    keyGen(**vars(args))
