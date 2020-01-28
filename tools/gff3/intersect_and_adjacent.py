6df839d5a956c722707bec357a88ecea9286598f
Anthony C <anthonyc@tamu.edu>
2020-01-28 11:14:44 -0600

    Adding stranding and fixing window for adjacency

--- tools/gff3/intersect_and_adjacent.py
+++ tools/gff3/intersect_and_adjacent.py
@@ -9,13 +9,13 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
 
-def treeFeatures(features):
+def treeFeatures(features, window):
     for feat in features:
         # Interval(begin, end, data)
-        yield Interval(int(feat.location.start), int(feat.location.end), feat.id)
+        yield Interval(int(feat.location.start) - int(window), int(feat.location.end) + int(window), feat.id)
 
 
-def intersect(a, b, window):
+def intersect(a, b, window, stranding):
     rec_a = list(GFF.parse(a))
     rec_b = list(GFF.parse(b))
     if len(rec_a) > 0 and len(rec_b) > 0:
@@ -23,12 +23,37 @@ def intersect(a, b, window):
         if len(rec_a) > 1 or len(rec_b) > 1:
                 raise Exception("Cannot handle multiple GFF3 records in a file, yet")
 
+        
         rec_a = rec_a[0]
         rec_b = rec_b[0]
 
+        a_neg = []
+        a_pos = []
+        b_neg = []
+        b_pos = []
+        if stranding == True:
+          for feat in rec_a.features:
+            if feat.strand > 0:
+              a_pos.append(Interval(int(feat.location.start) - int(window), int(feat.location.end) + int(window), feat.id))
+            else:
+              a_neg.append(Interval(int(feat.location.start) - int(window), int(feat.location.end) + int(window), feat.id))
+
+          for feat in rec_b.features:
+            if feat.strand > 0:
+              b_pos.append(Interval(int(feat.location.start) - int(window), int(feat.location.end) + int(window), feat.id))
+            else:
+              b_neg.append(Interval(int(feat.location.start) - int(window), int(feat.location.end) + int(window), feat.id))
+
+        
+        if stranding == False:        
         #builds interval tree from Interval objects of form (start, end, id) for each feature
-        tree_a = IntervalTree(list(treeFeatures(rec_a.features)))
-        tree_b = IntervalTree(list(treeFeatures(rec_b.features)))
+          tree_a = IntervalTree(list(treeFeatures(rec_a.features, window)))
+          tree_b = IntervalTree(list(treeFeatures(rec_b.features, window)))
+        else:
+          tree_a_pos = IntervalTree(a_pos)
+          tree_a_neg = IntervalTree(a_neg)
+          tree_b_pos = IntervalTree(b_pos)
+          tree_b_neg = IntervalTree(b_neg)
 
         #Used to map ids back to features later
         rec_a_map = {f.id: f for f in rec_a.features}
@@ -37,21 +62,45 @@ def intersect(a, b, window):
         rec_a_hits_in_b = []
         rec_b_hits_in_a = []
 
+
+        
+
+
         for feature in rec_a.features:
             #Save each feature in rec_a that overlaps a feature in rec_b
             #hits = tree_b.find_range((int(feature.location.start), int(feature.location.end)))
-            hits = tree_b[int(feature.location.start):int(feature.location.end)]
-            #feature id is saved in interval result.data, use map to get full feature
-            for hit in hits:
+
+            if stranding == False:
+              hits = tree_b[int(feature.location.start):int(feature.location.end)]
+
+              #feature id is saved in interval result.data, use map to get full feature
+              for hit in hits:
+                rec_a_hits_in_b.append(rec_b_map[hit.data])
+      
+            else:
+              hits_pos = tree_b_pos[int(feature.location.start):int(feature.location.end)]
+              hits_neg = tree_b_neg[int(feature.location.start):int(feature.location.end)]
+              for hit in hits_pos:
+                rec_a_hits_in_b.append(rec_b_map[hit.data])
+              for hit in hits_neg:
                 rec_a_hits_in_b.append(rec_b_map[hit.data])
 
         for feature in rec_b.features:
-            #Save each feature in rec_a that overlaps a feature in rec_b
-            #hits = tree_a.find_range((int(feature.location.start), int(feature.location.end)))
-            hits = tree_a[int(feature.location.start):int(feature.location.end)]
-            #feature id is saved in interval result.data, use map to get full feature
-            for hit in hits:
+            if stranding == False:
+              hits = tree_a[int(feature.location.start):int(feature.location.end)]
+
+              #feature id is saved in interval result.data, use map to get full feature
+              for hit in hits:
                 rec_b_hits_in_a.append(rec_a_map[hit.data])
+      
+            else:
+              hits_pos = tree_a_pos[int(feature.location.start):int(feature.location.end)]
+              hits_neg = tree_a_neg[int(feature.location.start):int(feature.location.end)]
+              for hit in hits_pos:
+                rec_b_hits_in_a.append(rec_a_map[hit.data])
+              for hit in hits_neg:
+                rec_b_hits_in_a.append(rec_a_map[hit.data])
+
 
         #Remove duplicate features using sets
         rec_a.features = set(rec_a_hits_in_b)
@@ -69,11 +118,12 @@ if __name__ == '__main__':
     parser.add_argument('a', type=argparse.FileType("r"))
     parser.add_argument('b', type=argparse.FileType("r"))
     parser.add_argument('window', type=int, default = 50, help = "Allows features this far away to still be considered 'adjacent'")
+    parser.add_argument('-stranding', action='store_true', help='Only allow adjacency for same-strand features')
     parser.add_argument('--oa', type=str, default='a_hits_near_b.gff')
     parser.add_argument('--ob', type=str, default='b_hits_near_a.gff')
     args = parser.parse_args()
 
-    b, a = intersect(args.a, args.b, args.window)
+    b, a = intersect(args.a, args.b, args.window, args.stranding)
 
     with open(args.oa, 'w') as handle:
         GFF.write([a], handle)
