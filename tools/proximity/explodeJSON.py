3dbed3733e6c26c563923705e3a4c0c762d483c9
unknown <cross12tamu@gmail.com>
2020-02-12 16:21:37 -0600

    json parser for data moving between scripts ; #309

---
+++ tools/proximity/explodeJSON.py
@@ -0,0 +1,24 @@
+import json
+
+class explodeJSON:
+
+    def __init__(self, file):
+        self.file = file
+
+    def readjson(self):
+        """ returns dictionary object for reading a JSON """
+        with open(self.file) as j:
+            myObj = json.load(j) 
+        
+        return myObj
+
+if __name__ == "__main__":
+    query = []
+    filepath = 'tools/proximity/test-data/'
+    filename = 'test.json'
+    e = explodeJSON(file=filepath+filename)
+    data = e.readjson()
+    print(data)
+    for k, v in data.items():
+        for term in v:
+            print(k+ ':' +term)
