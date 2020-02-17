ab1b46e8e0db18f13a3b4fec7f1851e2d2752638
Anthony C <anthonyc@tamu.edu>
2020-02-17 17:09:29 -0600

    Debug of TMD

--- tools/spanin/spaninFuncs.py
+++ tools/spanin/spaninFuncs.py
@@ -58,6 +58,8 @@ def find_tmd(pair,minimum=10,maximum=30,TMDmin=10,TMDmax=20):
     #print(search_region) # for trouble shooting
     
     print(search_region)
+    print(TMDmin)
+    print(TMDmax)
     print("Doing TMSIZE of bounds " + str(TMDmin) + " to " + str(TMDmax+1))
     for tmsize in range(TMDmin, TMDmax+1, 1):
         #print('==============='+str(tmsize)+'================') # print for troubleshooting
