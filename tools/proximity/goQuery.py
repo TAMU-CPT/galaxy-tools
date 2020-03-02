200c3ed0ac810be54e0b3668c1854ed30eb65d73
cross12tamu <cross12tamu@gmail.com>
2020-03-02 08:52:46 -0600

    finished pipeline for fetching go terms from a quickgo search

--- tools/proximity/goQuery.py
+++ tools/proximity/goQuery.py
@@ -1,6 +1,7 @@
 import synonymParse as sp
 import requests, sys
 import json
+import argparse
 
 class GOQ:
     """
@@ -42,11 +43,9 @@ class GOQ:
                 sys.exit()
 
             term_data = r.json()
-            #print(go_term)
-            #print('++++++++++++++')
+
             for go_term_return in term_data['results']: 
                 if go_term_return['isObsolete'] == True:
-                    print('im here')
                     go_data.pop(go_term)
                 else:
                     go_data[go_term]['id'] = go_term_return['id']
@@ -54,49 +53,91 @@ class GOQ:
                     go_data[go_term]['obsoletion'] = go_term_return['isObsolete']
                     go_data[go_term]['definition'] = go_term_return['definition']['text']
                     go_data[go_term]['usage'] = go_term_return['usage']
-                    #print(go_term_return)
+
                     try:
+                        go_data[go_term]['synonyms'] = []
                         for each_syn in go_term_return['synonyms']:
-                            #print(each_syn)
+
                             if each_syn['type'] == 'exact':
-                                go_data[go_term]['synonyms'] = [each_syn['name']]
+
+                                go_data[go_term]['synonyms'] += [each_syn['name']]
                             else:
-                                go_data[go_term]['synonyms'] = ['none to report']
+
                                 continue
-                        #print(go_term_return)
-                        #print('==========')
+                        if go_data[go_term]['synonyms'] == []:
+                            go_data[go_term]['synonyms'] = ['None'] # states that there were no "exact" matching synonyms
                     except KeyError:
-                        go_data[go_term]['synonyms'] = ['none to report']
-        #{k: v for k, v in go_data.items() if v is not None}
+                        go_data[go_term]['synonyms'] = ['NoneAtAll'] # Means that the return data from GO stated that there were NO synonyms
+
         return go_data
         
     def store_queries(self):
-        # What I want is each object that is made from query_gos to be passed to a new dict featuring {initial query : {return results}}
+        ''' Self checking ... can always check this dictionary if the go terms match (value of intial dict key to id value)'''
         the_queries = {}
         the_queries[self.term] = self.query_gos()
-            
-        print(the_queries)
-        print('\n')
-        print('The size of queries is: '+str(len(the_queries)))
-        for k, v in the_queries.items():
-            print(k+' : ')
-            print('\n')
-            for kk, vv in v.items():
-                print(kk+' : '+str(vv['name']))
-                print(vv['synonyms'])
+        return the_queries
 
 
-    def write_list_of_dicts(self):
-        pass
+    def write_list_of_dicts(self, filename):
+        
+        query_dict = self.store_queries()
+        with filename as f:
+            f.write('{}\t{}\t{}\t{}\t{}\t{}\n'.format('query_term','go_id','name','go_definition','usage','synonyms'))
+            for search_item, matches in query_dict.items():
+                for go_term, data in matches.items():
+                    f.write('{}\t{}\t{}\t{}\t{}\t'.format(search_item, data['id'],data['name'],data['definition'],data['usage']))
+                    if len(data['synonyms']) <= 1:
+                        f.write(str(data['synonyms'][0])+'\n')
+                    else:
+                        for synonym in data['synonyms']:
+                            if synonym == data['synonyms'][-1]:
+                                f.write("{}\n".format(synonym)) # last one doesn't need pipe separator
+                            else:
+                                f.write("{}|".format(synonym))
+
+                #print('\n')
 '''
 ############################################################### TEST RANGE / SCRIPT RANGE ##################################################
 '''
 if __name__ == "__main__":
-    my_interest = ['lysis','endolysin','amidase']
+
+    # Parameters
+    parser = argparse.ArgumentParser(description='Gene Ontology (GO) term and synonym returns from an input query')
+    parser.add_argument('--fileType', choices=('database','new line txt file', 'other'), default='new line txt file', help='help the script decide what kind of file it is going to analyze') # input file or list
+    parser.add_argument('file', type=argparse.FileType('r'), help='Input File') # Input file --> if testing, use syn.txt
+    parser.add_argument('--output', dest='output', type=argparse.FileType('w'), default='go-synonym-results.txt', help='Name of the output file') # output file name
+
+    args = parser.parse_args()
+
+    if args.fileType == 'database':
+        pass
+    elif args.fileType == 'new line txt file':
+        terms_to_search = []
+        with args.file as f:
+            for line in f.readlines():
+                line = line.split()[0]
+                print(line)
+                terms_to_search.append(line)
+        for term in terms_to_search:
+            if term == terms_to_search[0]:
+                print(term)
+                g = GOQ(term=term)
+                g.write_list_of_dicts(filename=args.output)
+            else:
+                filename = args.output.name
+                filename = open(filename, 'a+')
+                g = GOQ(term=term)
+                g.write_list_of_dicts(filename=filename)
+
+    elif args.fileType == 'other':
+        pass
+'''
+    my_interest = ['lysis']#,'endolysin','amidase']
     for item in my_interest:
         g = GOQ(term=item)
         #list_of_gos = g.search_gos()
         #print(list_of_gos)
         #g.query_gos()
-        g.store_queries()
+        g.write_list_of_dicts()
+'''
 
