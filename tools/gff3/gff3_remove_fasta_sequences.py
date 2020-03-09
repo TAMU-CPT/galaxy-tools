c7d4a92ff8b0157b3f6f299f45ddb02b248f237e
ahha14215 <ash3.holt@gmail.com>
2020-03-09 13:53:08 -0500

    Simplify remove_fasta_seq() logic, change filetype

--- tools/gff3/gff3_remove_fasta_sequences.py
+++ tools/gff3/gff3_remove_fasta_sequences.py
@@ -7,19 +7,13 @@ import argparse
 
 
 def remove_fasta_seq(gff3, ogff3):
-    i = 0
-    stop = float("inf")
-    # creates output file
-    with open(ogff3, "w") as handle:
-        # iterates ilne by line through input
-        for line in gff3:
-            i += 1
-            # finds the '##FASTA' line, and line number becomes 'stop'
-            if line.startswith("##FASTA"):
-                stop = i
-            # only writes lines to output if before stop
-            if i < stop:
-                handle.write(line)
+    # iterates line by line through input
+    for line in gff3:
+        # writes lines to output until ##FASTA
+        if line.startswith("##FASTA"):
+            return
+        else:
+            ogff3.write(line)
 
 
 if __name__ == "__main__":
@@ -27,8 +21,8 @@ if __name__ == "__main__":
         description="Identify lysis gene candidates next to possible endolysin or holin genes",
         epilog="",
     )
-    parser.add_argument("gff3", type=argparse.FileType("rw"), help="original gff3 file")
-    parser.add_argument("--ogff3", type=str, default="output.gff3")
+    parser.add_argument("gff3", type=argparse.FileType("r"), help="original gff3 file")
+    parser.add_argument("ogff3", type=argparse.FileType("w"), default="output.gff3")
     args = parser.parse_args()
 
     remove_fasta_seq(args.gff3, args.ogff3)
