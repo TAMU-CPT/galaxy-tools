24e1401203db16678a31bbbd2b086dcb52364803
cross12tamu <cross12tamu@gmail.com>
2020-03-18 15:07:09 -0500

    comment out module

--- tools/helicalWheel/plotWheels/helical_wheel.py
+++ tools/helicalWheel/plotWheels/helical_wheel.py
@@ -1,7 +1,7 @@
 import matplotlib.lines as lines
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+#from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from scipy.stats.kde import gaussian_kde
 
