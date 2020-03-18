45d417a70d1e86f69caf1f3f3af5589c2da53983
Cory Maughmer <cory.maughmer@tamu.edu>
2020-03-18 15:20:09 -0500

    formatted with black

--- tools/blast/blastp_to_gff3.py
+++ tools/blast/blastp_to_gff3.py
@@ -14,6 +14,7 @@ BlastXML files, when transformed to GFF3, do not normally show gaps in the
 blast hits. This tool aims to fill that "gap".
 """
 
+
 def blast2gff3(blast, blastxml=False, blasttab=False, include_seq=False):
     # Call correct function based on xml or tabular file input, raise error if neither or both are provided
     if blastxml and blasttab:
@@ -24,15 +25,17 @@ def blast2gff3(blast, blastxml=False, blasttab=False, include_seq=False):
         return recs
     elif blasttab:
         raise Exception("Tabular not supported yet, please use xml")
-        #yield blasttab2gff3(blast, include_seq)
+        # yield blasttab2gff3(blast, include_seq)
     else:
         raise Exception("Must provide either blast xml or tabular file")
+
 
 def blastxml2gff3(blastxml, include_seq=False):
     from Bio.Blast import NCBIXML
     from Bio.Seq import Seq
     from Bio.SeqRecord import SeqRecord
     from Bio.SeqFeature import SeqFeature, FeatureLocation
+
     blast_records = NCBIXML.parse(blastxml)
     for idx_record, record in enumerate(blast_records):
         # http://www.sequenceontology.org/browser/release_2.4/term/SO:0000343
@@ -51,14 +54,14 @@ def blastxml2gff3(blastxml, include_seq=False):
             parent_match_start = 0
             parent_match_end = 0
             hit_qualifiers = {
-                    "ID": "b2g.%s.%s.%s" % (idx_record, idx_hit, '0'),
-                    "source": "blast",
-                    "accession": hit.accession,
-                    "hit_id": hit.hit_id,
-                    "length": hit.length,
-                    "hit_titles": hit.title.split(" >"),
-                    "hsp_count": len(hit.hsps),
-                    }
+                "ID": "b2g.%s.%s.%s" % (idx_record, idx_hit, "0"),
+                "source": "blast",
+                "accession": hit.accession,
+                "hit_id": hit.hit_id,
+                "length": hit.length,
+                "hit_titles": hit.title.split(" >"),
+                "hsp_count": len(hit.hsps),
+            }
             sub_features = []
             for idx_hsp, hsp in enumerate(hit.hsps):
                 if idx_hsp == 0:
@@ -103,10 +106,10 @@ def blastxml2gff3(blastxml, include_seq=False):
                 desc = hit.title.split(" >")[0]
                 hsp_qualifiers["description"] = desc[desc.index(" ") :]
 
-                #check if parent boundary needs to increase to envelope hsp
+                # check if parent boundary needs to increase to envelope hsp
                 if hsp.query_start < parent_match_start:
                     parent_match_start = hsp.query_start - 1
-                if hsp.query_end > parent_match_end:    
+                if hsp.query_end > parent_match_end:
                     parent_match_end = hsp.query_end + 1
 
                 # add hsp to the gff3 feature as a "match_part"
@@ -134,10 +137,9 @@ def blastxml2gff3(blastxml, include_seq=False):
         yield rec
 
 
-def blasttsv2gff3(
-    blasttsv, include_seq=False 
-):
+def blasttsv2gff3(blasttsv, include_seq=False):
     yield None
+
 
 def blasttab2gff3(blasttab, include_seq=False):
     yield None
@@ -145,16 +147,21 @@ def blasttab2gff3(blasttab, include_seq=False):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Convert Blast output to gapped GFF3, must provide XML or Tabular output", epilog=""
+        description="Convert Blast output to gapped GFF3, must provide XML or Tabular output",
+        epilog="",
     )
     parser.add_argument(
-            "blast", type=argparse.FileType("r"), help="Blast XML or 25 Column Tabular Output file"
+        "blast",
+        type=argparse.FileType("r"),
+        help="Blast XML or 25 Column Tabular Output file",
     )
     parser.add_argument(
         "--blastxml", action="store_true", help="Process file as Blast XML Output"
     )
     parser.add_argument(
-        "--blasttab", action="store_true", help="Process file as Blast 25 Column  Tabular Output"
+        "--blasttab",
+        action="store_true",
+        help="Process file as Blast 25 Column  Tabular Output",
     )
     parser.add_argument("--include_seq", action="store_true", help="Include sequence")
     args = parser.parse_args()
