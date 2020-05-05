bbcefedb1b5acc3e94d61817fd690857bbfde62b
curtisross <curtisross@tamu.edu>
2020-05-05 17:19:17 -0500

    made an awk command to help with empty lines

--- tools/efetch/helperFunctions.py
+++ tools/efetch/helperFunctions.py
@@ -1,19 +1,25 @@
 import subprocess
 import os
 
-def cat_files(file_ext, output="combination.txt"):
+def cat_files(file_ext, output="cat_combination.txt",galaxy=False):
     """ concatenates files together """
-    cwd = os.getcwd()
-    print(cwd)
+    #cwd = os.getcwd()
     try:
         cmd = "cat *.{} > {}".format(file_ext,output)
         subprocess.run(cmd, shell=True)#, stdout=output)
     except subprocess.TimeoutExpired as err:
         print(err)
-
+        
+def awk_files(file_ext, output="awk_combination.txt",galaxy=False):
+    try:
+        cmd = "awk NF *.{} > {}".format(file_ext, output)
+        subprocess.run(cmd, shell=True)
+    except subprocess.TimeoutExpired as err:
+        print(err)
 def clean_files(input="combination.txt"):
     print("START HERE WITH REMOVING WHITE SPACE FROM ODD ADDITIONS")
 
 if __name__ == "__main__":
-    cat_files("fasta")
-    clean_files()
+    #cat_files("fasta")
+    #clean_files()
+    awk_files("DAT")
