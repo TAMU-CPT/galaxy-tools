aa972186db800a1812b96e2649455d55e2c3f81c
curtisross <curtisross@tamu.edu>
2020-05-06 16:32:57 -0500

    new subprocess func

--- tools/efetch/helperFunctions.py
+++ tools/efetch/helperFunctions.py
@@ -9,15 +9,23 @@ def cat_files(file_ext, output="cat_combination.txt",galaxy=False):
         subprocess.run(cmd, shell=True)#, stdout=output)
     except subprocess.TimeoutExpired as err:
         print(err)
-        
+
+
 def awk_files(file_ext, output="awk_combination.txt",galaxy=False):
     try:
         cmd = "awk NF *.{} > {}".format(file_ext, output)
         subprocess.run(cmd, shell=True)
     except subprocess.TimeoutExpired as err:
         print(err)
-def clean_files(input="combination.txt"):
-    print("START HERE WITH REMOVING WHITE SPACE FROM ODD ADDITIONS")
+
+
+def pass_flag(input,flag="--output"):
+    try:
+        cmd = "{} {}".format(flag,input)
+        subprocess.run(cmd,shell=True)
+    except subprocess.TimeoutExpired as err:
+        print(err)
+
 
 if __name__ == "__main__":
     #cat_files("fasta")
