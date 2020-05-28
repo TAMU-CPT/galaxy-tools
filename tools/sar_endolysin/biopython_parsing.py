7afd0d0e77e7bc5e14d79f8654c53161d978e0cd
curtisross <curtisross@tamu.edu>
2020-05-28 15:21:56 -0500

    biopython parsing, currently only multi fasta

---
+++ tools/sar_endolysin/biopython_parsing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# Biopython parsing module. Uses in conjunction with the sar_finder script, and potential future scripts down the line.
+
+from Bio import SeqIO
+
+class FASTA_parser:
+    """ Parses multi fasta file, and zips together header with sequence """
+
+    def __init__(self, fa):
+        self.fa = fa
+    
+    def parse_and_dict_multifasta(self):
+        """ parses the input multi fasta, and puts results into dictionary """
+
+        return SeqIO.to_dict(SeqIO.parse(self.fa,"fasta"))
+
+
+if __name__ == "__main__":
+    fa_file = "test-data/mu-proteins.fa"
+    d = FASTA_parser(fa_file).parse_and_dict_multifasta()
+    print(d)
+    
