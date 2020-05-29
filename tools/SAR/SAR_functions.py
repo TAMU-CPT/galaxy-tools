a6475827ccbe21187c85caf61c239a7519a93828
curtisross <curtisross@tamu.edu>
2020-05-29 11:43:22 -0500

    snorkel catch added, various script reorg; #356

--- tools/SAR/SAR_functions.py
+++ tools/SAR/SAR_functions.py
@@ -4,10 +4,8 @@ import sys
 import argparse
 import os
 import re
-import pandas as pd
-import numpy as np
-from biopython_parsing import FASTA_parser
 from Bio import SeqIO
+
 
 class CheckSequence:
     """ SAR endolysin Verification class, which starts with complete FA file, and is shrunk by each function to reveal best candidates of SAR endolysin proteins """
@@ -31,7 +29,7 @@ class CheckSequence:
 
     def check_hydrophobicity(self,tmd_min=15,tmd_max=20):
         """ verifies the existence of a hydrophobic region within the sequence """
-        hydrophobic_residues = "['FIWLVMYCATGS']"
+        hydrophobic_residues = "['FIWLVMYCATGS']" # fed through regex
         hits = self.store
         if self.size > 50:
             seq = self.seq[0:50]
@@ -41,46 +39,34 @@ class CheckSequence:
             for i in range(0,len(seq)-tmd_size,1):
                 check_seq = str(seq[i:i+tmd_size]) # fix this tomorrow
                 if re.search((hydrophobic_residues+"{"+str(tmd_size)+"}"),check_seq):
-                    if self.name not in hits:
-                        hits[self.name] = {}
-                        hits[self.name]["description"] = str(self.description)
-                        hits[self.name]["sequence"] = str(self.seq)
-                        hits[self.name]["size"] = str(self.size)
-                        if "TMD_"+str(tmd_size) not in hits[self.name]:
-                            hits[self.name]["TMD_"+str(tmd_size)] = []
-                            hits[self.name]["TMD_"+str(tmd_size)].append([check_seq])
-                        else:
-                            hits[self.name]["TMD_"+str(tmd_size)].append([check_seq])
-                    continue
-                elif "K" in check_seq[0] and re.search((hydrophobic_residues+"{"+str(tmd_size)-1+"}"),check_seq[1:]): # check frontend snorkels
-                    if self.name not in hits:
-                        hits[self.name] = {}
-                        hits[self.name]["description"] = str(self.description)
-                        hits[self.name]["sequence"] = str(self.seq)
-                        hits[self.name]["size"] = str(self.size)
-                        if "TMD_"+str(tmd_size) not in hits[self.name]:
-                            hits[self.name]["TMD_"+str(tmd_size)] = []
-                            hits[self.name]["TMD_"+str(tmd_size)].append([check_seq])
-                        else:
-                            hits[self.name]["TMD_"+str(tmd_size)].append([check_seq])
-                    continue
-                elif "K" in check_seq[-1] and re.search((hydrophobic_residues+"{"+str(tmd_size)-1+"}"),check_seq[:-1]): # check backend snorkels
-                    if self.name not in hits:
-                        hits[self.name] = {}
-                        hits[self.name]["description"] = str(self.description)
-                        hits[self.name]["sequence"] = str(self.seq)
-                        hits[self.name]["size"] = str(self.size)
-                        if "TMD_"+str(tmd_size) not in hits[self.name]:
-                            hits[self.name]["TMD_"+str(tmd_size)] = []
-                            hits[self.name]["TMD_"+str(tmd_size)].append([check_seq])
-                        else:
-                            hits[self.name]["TMD_"+str(tmd_size)].append([check_seq])
-                    continue
-                else:
-                    continue
+                    storage_dict(self=self,tmd_size=tmd_size,check_seq=check_seq,hits=hits)
+                elif "K" in check_seq[0] and re.search((hydrophobic_residues+"{"+str(tmd_size-1)+"}"),check_seq[1:]): # check frontend snorkels
+                    storage_dict(self=self,tmd_size=tmd_size,check_seq=check_seq,hits=hits)
+                elif "K" in check_seq[-1] and re.search((hydrophobic_residues+"{"+str(tmd_size-1)+"}"),check_seq[:-1]): # check backend snorkels
+                    storage_dict(self=self,tmd_size=tmd_size,check_seq=check_seq,hits=hits)
+                continue
         
         return hits
 
+
+### Extra "helper" functions
+def storage_dict(self,tmd_size,check_seq,hits):
+    if self.name not in hits:
+        hits[self.name] = {}
+        hits[self.name]["description"] = str(self.description)
+        hits[self.name]["sequence"] = str(self.seq)
+        hits[self.name]["size"] = str(self.size)
+        if "TMD_"+str(tmd_size) not in hits[self.name]:
+            hits[self.name]["TMD_"+str(tmd_size)] = []
+            hits[self.name]["TMD_"+str(tmd_size)].append([check_seq])
+        else:
+            hits[self.name]["TMD_"+str(tmd_size)].append([check_seq])
+    else:
+        if "TMD_"+str(tmd_size) not in hits[self.name]:
+            hits[self.name]["TMD_"+str(tmd_size)] = []
+            hits[self.name]["TMD_"+str(tmd_size)].append([check_seq])
+        else:
+            hits[self.name]["TMD_"+str(tmd_size)].append([check_seq]) 
 
 if __name__ == "__main__":
     pass
