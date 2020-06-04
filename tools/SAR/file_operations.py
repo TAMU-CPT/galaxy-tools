191d0a4dda8abbd806669c0577c191479f8ac287
curtisross <curtisross@tamu.edu>
2020-06-03 20:46:38 -0500

    tweaked txt file output; not needed for endgame

--- tools/SAR/file_operations.py
+++ tools/SAR/file_operations.py
@@ -6,7 +6,11 @@ def fasta_from_SAR_dict(sar_dict,fa_file):
             f.writelines(">{}\n".format(data["description"]))
             f.writelines("{}\n".format(data["sequence"]))
 
-def stat_file_from_SAR_dict(sar_dict, stat_file, tmd_min, tmd_max):
+def gff3_from_SAR_dict(sar_dict,gff3_file):
+    """ make a multi gff3 with candidates from SAR dictionary """
+    pass
+
+def stat_file_from_SAR_dict(sar_dict, stat_file, sar_min, sar_max):
     """ summary statistics from SAR finder function """
     with stat_file as f:
         f.writelines("..........:::::: Candidate SAR Proteins ::::::..........\n\n")
@@ -16,14 +20,19 @@ def stat_file_from_SAR_dict(sar_dict, stat_file, tmd_min, tmd_max):
                 f.writelines("Protein Sequence: {}\n".format(data["sequence"]))
                 f.writelines("Protein Length: {}\n".format(data["size"]))
                 f.writelines("SAR Criteria matching region(s)\n")
-                for tmd_size in range(tmd_max, tmd_min-1, -1):
+                for tmd_size in range(sar_max, sar_min-1, -1):
                     if "TMD_"+str(tmd_size) in data:
                         f.writelines("\nSAR length of {}:\n".format(tmd_size))
                         for each_match in data["TMD_"+str(tmd_size)]:
-                            f.writelines("Potential SAR domain sequence: {}\n".format(each_match[0]))
+                            f.writelines("\nPotential SAR domain sequence: {}\n".format(each_match[0]))
                             f.writelines("N-term sequence: {}\n".format(each_match[1]))
                             f.writelines("N-term net charge: {}\n".format(each_match[2]))
-                            f.writelines("Percent GA content: {:.2f}%\n".format(each_match[3]))
+                            for each_perc_calc in each_match[3]:
+                                f.writelines("Percent {} content: {}%\n".format(each_perc_calc[0],each_perc_calc[1]))
+                            f.writelines("N-term coords: {}\n".format(each_match[4]))
+                            f.writelines("SAR coords: {}\n".format(each_match[5]))
+                            f.writelines("C-term coords: {}\n".format(each_match[6]))
+                            f.writelines("SAR start: {}\n".format(each_match[7]))
                 f.writelines("========================================================\n\n")
         else:
             f.writelines("No candidate SAR Proteins found")
