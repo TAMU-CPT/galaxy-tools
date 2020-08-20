ab4606934504cd391435b005e1d02e9eee9ae6cc
Cory Maughmer <cory.maughmer@tamu.edu>
2020-08-20 11:51:50 -0500

    updated for new aragorn tool

--- tools/external/fix-aragorn-gff3.py
+++ tools/external/fix-aragorn-gff3.py
@@ -17,7 +17,10 @@ def fixed_feature(rec):
         )
     ):
         fid = "tRNA-%03d" % (1 + idx)
-        name = ["tRNA-" + feature.qualifiers["Codon"][0]]
+        try:
+            name = ["tRNA-" + feature.qualifiers["Codon"][0]]
+        except KeyError:
+            name = [feature.qualifiers['product'][0]]
         gene = SeqFeature(
             location=feature.location,
             type="gene",
