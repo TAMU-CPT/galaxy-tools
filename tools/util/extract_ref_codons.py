9b4c37d158e0f3543d08aa72f5cf00c0912c1332
Anthony C <anthonyc@tamu.edu>
2020-09-24 11:11:43 -0500

    Python3 compatibility

--- tools/util/extract_ref_codons.py
+++ tools/util/extract_ref_codons.py
@@ -22,7 +22,7 @@ def extractCodonsFromDb(target, codondb):
             break
 
     if header is None:
-        print "Not found"
+        print("Not found")
         sys.exit(1)
 
     spsum_label = (
@@ -46,6 +46,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     ocu = extractCodonsFromDb(**vars(args))
-    print "# Codon\tCount"
+    print ("# Codon\tCount")
     for (key, value) in ocu.iteritems():
-        print "%s\t%s" % (key, value)
+        print ("%s\t%s" % (key, value))
