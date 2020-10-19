0fc1d5081e7a695741a020c9b774f4a6398aec89
Anthony C <anthonyc@tamu.edu>
2020-10-19 15:55:47 -0500

    Attempt to automatically find parents of unparented sub-features

--- tools/gff3/gbk_to_gff3.py
+++ tools/gff3/gbk_to_gff3.py
@@ -7,6 +7,8 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from cpt_gffParser import gffSeqFeature, gffWrite
 
+bottomFeatTypes = ["exon", "RBS", "CDS"]
+
 def makeGffFeat(inFeat, num, recName):
     if "codon_start" in inFeat.qualifiers.keys():
       shift = int(inFeat.qualifiers["codon_start"][0]) - 1
@@ -52,9 +54,13 @@ def main():
         locBucket = {}
         outFeats = []
         topTypeDict = {}
+        seekingParent = []
         for feat in rec.features:
             if "locus_tag" not in feat.qualifiers.keys(): #Allow metadata features and other features with no ID (Output warning?) - AJC
-              if feat.type not in topTypeDict.keys():
+              if feat.type in bottomFeatTypes:
+                seekingParent.append([feat, [], []]) # [Feature, all parent candidates, strongest parent candidates]
+                continue
+              elif feat.type not in topTypeDict.keys():
                 topTypeDict[feat.type] = 1
               else:
                 topTypeDict[feat.type] += 1
@@ -64,6 +70,73 @@ def main():
               locBucket[feat.qualifiers["locus_tag"][0]] = []
             locBucket[feat.qualifiers["locus_tag"][0]].append(feat)
 
+        for locus in locBucket.keys():
+          minLoc = locBucket[locus][0].location.start
+          maxLoc = locBucket[locus][0].location.end
+          for feat in locBucket[locus]:
+            minLoc = min(minLoc, feat.location.start)
+            maxLoc = max(maxLoc, feat.location.end)
+          for x in seekingParent:
+            if x[0].location.start >= minLoc and x[0].location.end <= maxLoc:
+              x[1].append(locus)
+            if x[0].location.start == minLoc or x[0].location.end == maxLoc:
+              x[2].append(locus)
+
+        for x in seekingParent: #Reformat to [Feature, Locus, Unused/Free]
+          if len(x[2]) == 1:
+            finList = ""
+            if len(x[1]) > 1:
+              for loc in x[1]:
+                if loc != x[2][0]:
+                  finList += loc + ", "
+              finList = str(x[0].type) + " had no locus tag set in .gbk file, automatically derived. Other, weaker candidate(s) were " + finList[0:-2] + "."
+            else:
+              finList = str(x[0].type) + " had no locus tag set in .gbk file, automatically derived."
+            if "Notes" not in x[0].qualifiers.keys():
+              x[0].qualifiers["Notes"] = []
+            x[0].qualifiers["Notes"].append(finList)
+            x[1] = x[2][0]
+          elif len(x[2]) > 1:
+            candidate = x[2][0] #Arbitrarily choose first one
+            finList = ""
+            strongList = ""
+            for loc in x[2]:
+              if loc != candidate:
+                finList += loc + ", "
+                strongList += loc + ", "
+            for loc in x[1]:
+              if loc not in x[2]:
+                finList += loc + ", "
+            finList = str(x[0].type) + " had no locus tag set in .gbk file, automatically derived. Other candidate(s) were " + finList[0:-2] + " (Equally strong candidate(s): " + strongList[0:-2] + ")."
+            if "Notes" not in x[0].qualifiers.keys():
+              x[0].qualifiers["Notes"] = []
+            x[0].qualifiers["Notes"].append(finList)
+            x[1] = candidate
+          elif len(x[1]) == 1:
+            x[1] = x[1][0]
+            if "Notes" not in x[0].qualifiers.keys():
+              x[0].qualifiers["Notes"] = []
+            finList = str(x[0].type) + " had no locus tag set in .gbk file, automatically derived."
+            x[0].qualifiers["Notes"].append(finList)
+          elif len(x[1]) > 1:
+            candidate = x[1][0] #Arbitrarily choose first one
+            finList = ""
+            for loc in x[1]:
+              if loc != candidate:
+                finList += loc + ", "
+            finList = str(x[0].type) + " had no locus tag set in .gbk file, automatically derived. Other candidates were " + finList[0:-2] + "."
+            if "Notes" not in x[0].qualifiers.keys():
+              x[0].qualifiers["Notes"] = []
+            x[0].qualifiers["Notes"].append(finList)
+            x[1] = candidate
+          else:
+            sys.stderr.write("Warning: Unable to find potential parent for feature with no locus tag of type " + str(x[0].type) + " at location [" + str(x[0].location.start) + ", " + str(x[0].location.end) + "].\n")
+            if x[0].type not in topTypeDict.keys():
+              topTypeDict[x[0].type] = 1
+            else:
+              topTypeDict[x[0].type] += 1
+            outFeats.append(makeGffFeat(x[0], topTypeDict[x[0].type], recID))
+            
         for locus in locBucket.keys():
           if len(locBucket[locus]) == 1: # No heirarchy to be made
             outFeats.append(makeGffFeat(locBucket[locus][0], 0, recID))
@@ -94,6 +167,19 @@ def main():
               else:
                 typeDict[feat.type] += 1 
               bottomFeats.append(feat)
+          
+          for x in seekingParent:
+            if type(x[1]) != "list" and locus == x[1]:
+              x[0].qualifiers["locus_tag"] = [locus]
+              bottomFeats.append(x[0])
+              if x[0].type not in typeDict.keys():
+                typeDict[x[0].type] = 1
+              else:
+                typeDict[x[0].type] += 1 
+              
+          
+            
+              
             
           #if not topFeat: # Make our own top-level feature based off minLoc, maxLoc bounds
             
