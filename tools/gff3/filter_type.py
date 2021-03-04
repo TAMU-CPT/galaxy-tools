8c356bd7be5f469e297d43e2b2fa0e8ec3b3188d
Anthony C <anthonyc@tamu.edu>
2021-03-04 12:20:54 -0600

    Fix for generator return

--- tools/gff3/filter_type.py
+++ tools/gff3/filter_type.py
@@ -12,11 +12,24 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     for rec in gffParse(args.gff3):
-        rec.features = feature_lambda(
+        tempFeats = feature_lambda(
             rec.features,
             feature_test_type,
             {"types": args.types},
             invert=args.invert,
             subfeatures=False,
         )
+        rec.features = []
+        for x in tempFeats:
+          rec.features.append(x)
+        for x in rec.features:
+          if "Parent" in x.qualifiers.keys():
+            found = 0
+            for seek in x.qualifiers["Parent"]:
+              for y in rec.features:
+                if y.id == seek:
+                  found += 1
+                  break
+            if found < len(x.qualifiers["Parent"]):
+              del x.qualifiers["Parent"]
         gffWrite([rec], sys.stdout)
