56d80ca3f1930da7aaf13de1bea6abff1158a186
Anthony C <anthonyc@tamu.edu>
2021-08-31 21:57:35 -0500

    Fix dedup relying on StringIO

--- tools/fasta/fasta_dedup.py
+++ tools/fasta/fasta_dedup.py
@@ -2,23 +2,23 @@
 import logging
 import copy
 import argparse
-import StringIO
 import hashlib
+import sys
 from Bio import SeqIO
 
 logging.basicConfig(level=logging.INFO)
 
 
-def dedup(fasta_file, mutation="mutate"):
+def dedup(fasta_file):
     records = list(SeqIO.parse(fasta_file, "fasta"))
-    output = StringIO.StringIO()
+    output = ""
 
     known_records = {}
     ordering_keys = []
 
     for record in records:
         seq = str(record.seq).upper()
-        md5 = hashlib.md5(seq).hexdigest()
+        md5 = hashlib.md5(seq.encode('utf-8')).hexdigest()
 
         if md5 in known_records:
             known_records[md5]["keys"].append(record.id)
@@ -30,8 +30,8 @@ def dedup(fasta_file, mutation="mutate"):
         if len(known_records[key]["keys"]) > 0:
             ident_str = ", ".join(known_records[key]["keys"])
             known_records[key]["rec"].description += " [Ident: %s]" % ident_str
-        SeqIO.write(known_records[key]["rec"], output, "fasta")
-    print(output.getvalue())
+        SeqIO.write(known_records[key]["rec"], sys.stdout, "fasta")
+    #print(output)
 
 
 if __name__ == "__main__":
