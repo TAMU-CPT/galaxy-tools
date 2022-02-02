bbd0bf117cdf4db970ebb2522721c5faa3bb100c
Anthony C <anthonyc@tamu.edu>
2022-02-02 12:09:47 -0600

    Accomodate multiple-ID for export

--- tools/webapollo/gga-apollo/delete_organism.py
+++ tools/webapollo/gga-apollo/delete_organism.py
@@ -35,7 +35,13 @@ if __name__ == '__main__':
     handle_credentials(gx_user)
 
     # Get organism
+    all_orgs = wa.organisms.get_organisms()
+    if 'error' in all_orgs:
+      raise Exception("Error while getting the list of organisms: %s" % all_orgs)
+        
+
     org_cn = []
+    #org_ids = []
     if args.org_json:
         org_cn = [x.get("commonName", None) for x in json.load(args.org_json)]
         org_cn = [x for x in orgs if x is not None]
@@ -43,44 +49,37 @@ if __name__ == '__main__':
         args.org_raw = str(args.org_raw)
         args.org_raw = args.org_raw.split(",")
         for i in range(len(args.org_raw)):
+            sys.stderr.write("Asserting " + args.org_raw[i].strip() + " to be deleted")
             org_cn.append(args.org_raw[i].strip())
     elif args.org_id:
         orgList = str(args.org_id)
         orgList = orgList.split(",")
-        for x in orgList:
-            sys.stderr.write("Asserting " + x.strip() + " to be deleted")
-            args.org_id = x.strip()
-            res = GuessOrg(args, wa)
-            if res:
-               sys.stderr.write("Assert passed.")
-               org_cn.append(res[0])
+        for orgInd in orgList:
+          foundOrg = False
+          orgInd = orgInd.strip()
+          sys.stderr.write("Asserting " + orgInd.strip() + " to be deleted")
+          for allOrgInd in all_orgs:
+            if str(allOrgInd["id"]) == orgInd:
+              org_cn.append(str(allOrgInd["commonName"]))
+              foundOrg = True
+              break        
+          if not foundOrg:
+            raise Exception("Organism '%s' not found" % orgInd)
 
     if len(org_cn) == 0:
         raise Exception("Organism Common Name not provided")
-
+    
     sys.stderr.write("CN List: " + str(org_cn))
             
 
-    all_orgs = wa.organisms.get_organisms()
-    if 'error' in all_orgs:
-        all_orgs = []
-    all_orgs = [org['commonName'] for org in all_orgs]
-    
-    for orgInd in org_cn:
-      noOrg = True
-      for eachOrg in all_orgs:
-        if eachOrg == orgInd:
-          noOrg = False
-          break
-      if noOrg:
-        raise Exception("Could not find organism %s" % orgInd)
-      else:
-        orgs = accessible_organisms(gx_user, orgInd, 'WRITE')    
-        if not orgs:
-          raise Exception("You do not have write permission on this organism")
+    # all_orgs = [org['commonName'] for org in all_orgs]
+    permdOrgs = accessible_organisms(gx_user, org_cn, 'WRITE')    
+    if len(permdOrgs) != len(org_cn):
+          raise Exception("You do not have write permission on one or more of the supplied organisms")
 
     for orgInd in org_cn:
       org = wa.organisms.show_organism(orgInd)
+      # Theoretically removable except direct entry would need a mechanism for getting ID in addition to common name
 
       wa.organisms.delete_features(org['id'])
  
--- tools/webapollo/gga-apollo/export.py
+++ tools/webapollo/gga-apollo/export.py
@@ -52,68 +52,57 @@ if __name__ == '__main__':
     if len(org_cn) == 0:
         raise Exception("Organism Common Name not provided")
 
-    #for orgInd in org_cn:
-    #  noOrg = True
-    #  for eachOrg in all_orgs:
-    #    if eachOrg == orgInd:
-    #      noOrg = False
-    #      break
-    #  if noOrg:
-    #    raise Exception("Could not find organism %s" % orgInd)
-    #  else:
-    #    orgs = accessible_organisms(gx_user, [orgInd], 'READ')    
-    #    if not orgs:
-    #      raise Exception("You do not have write permission on this organism")
-
-    orgs, seqs = GuessCn(args, wa)    
-    if len(orgs) != len(org_cn):
-      raise Exception("Missing Read permission on one or more submitted organism")
 
     org_data = []
     for org_ind in org_cn:
-        org = wa.organisms.show_organism(org_ind)
+          if args.org_raw:
+            args.org_raw = org_ind.strip()
+          elif args.org_id:
+            args.org_id = org_ind.strip()
+          orgs, seqs = GuessCn(args, wa)    
+          org = wa.organisms.show_organism(org_ind)
 
-        if args.gff:
-            uuid_gff = wa.io.write_downloadable(org['commonName'], 'GFF3', export_gff3_fasta=True, sequences=seqs)
-            if 'error' in uuid_gff or 'uuid' not in uuid_gff:
-                raise Exception("Apollo failed to prepare the GFF3 file for download: %s" % uuid_gff)
-            args.gff.write(wa.io.download(uuid_gff['uuid'], output_format="text"))
-            time.sleep(1)
+          if args.gff:
+              uuid_gff = wa.io.write_downloadable(org['commonName'], 'GFF3', export_gff3_fasta=True, sequences=seqs)
+              if 'error' in uuid_gff or 'uuid' not in uuid_gff:
+                  raise Exception("Apollo failed to prepare the GFF3 file for download: %s" % uuid_gff)
+              args.gff.write(wa.io.download(uuid_gff['uuid'], output_format="text"))
+              time.sleep(1)
 
-        if args.seq:
-            uuid_fa = wa.io.write_downloadable(org['commonName'], 'FASTA', sequences=seqs, seq_type='genomic')
-            if 'error' in uuid_fa or 'uuid' not in uuid_fa:
-                raise Exception("Apollo failed to prepare the FASTA file for download: %s" % uuid_fa)
-            args.seq.write(wa.io.download(uuid_fa['uuid'], output_format="text"))
-            time.sleep(1)
+          if args.seq:
+              uuid_fa = wa.io.write_downloadable(org['commonName'], 'FASTA', sequences=seqs, seq_type='genomic')
+              if 'error' in uuid_fa or 'uuid' not in uuid_fa:
+                  raise Exception("Apollo failed to prepare the FASTA file for download: %s" % uuid_fa)
+              args.seq.write(wa.io.download(uuid_fa['uuid'], output_format="text"))
+              time.sleep(1)
 
-        if args.vcf:
-            uuid_vcf = wa.io.write_downloadable(org['commonName'], 'VCF', sequences=seqs)
-            if 'error' in uuid_vcf or 'uuid' not in uuid_vcf:
-                raise Exception("Apollo failed to prepare the VCF file for download: %s" % uuid_vcf)
-            args.vcf.write(wa.io.download(uuid_vcf['uuid'], output_format="text"))
-            time.sleep(1)
+          if args.vcf:
+              uuid_vcf = wa.io.write_downloadable(org['commonName'], 'VCF', sequences=seqs)
+              if 'error' in uuid_vcf or 'uuid' not in uuid_vcf:
+                  raise Exception("Apollo failed to prepare the VCF file for download: %s" % uuid_vcf)
+              args.vcf.write(wa.io.download(uuid_vcf['uuid'], output_format="text"))
+              time.sleep(1)
 
-        if args.fasta_cdna:
-            uuid_fa = wa.io.write_downloadable(org['commonName'], 'FASTA', sequences=seqs, seq_type='cdna')
-            if 'error' in uuid_fa or 'uuid' not in uuid_fa:
-                raise Exception("Apollo failed to prepare the cdna FASTA file for download: %s" % uuid_fa)
-            args.fasta_cdna.write(wa.io.download(uuid_fa['uuid'], output_format="text"))
-            time.sleep(1)
+          if args.fasta_cdna:
+              uuid_fa = wa.io.write_downloadable(org['commonName'], 'FASTA', sequences=seqs, seq_type='cdna')
+              if 'error' in uuid_fa or 'uuid' not in uuid_fa:
+                  raise Exception("Apollo failed to prepare the cdna FASTA file for download: %s" % uuid_fa)
+              args.fasta_cdna.write(wa.io.download(uuid_fa['uuid'], output_format="text"))
+              time.sleep(1)
 
-        if args.fasta_cds:
-            uuid_fa = wa.io.write_downloadable(org['commonName'], 'FASTA', sequences=seqs, seq_type='cds')
-            if 'error' in uuid_fa or 'uuid' not in uuid_fa:
-                raise Exception("Apollo failed to prepare the cds FASTA file for download: %s" % uuid_fa)
-            args.fasta_cds.write(wa.io.download(uuid_fa['uuid'], output_format="text"))
-            time.sleep(1)
+          if args.fasta_cds:
+              uuid_fa = wa.io.write_downloadable(org['commonName'], 'FASTA', sequences=seqs, seq_type='cds')
+              if 'error' in uuid_fa or 'uuid' not in uuid_fa:
+                  raise Exception("Apollo failed to prepare the cds FASTA file for download: %s" % uuid_fa)
+              args.fasta_cds.write(wa.io.download(uuid_fa['uuid'], output_format="text"))
+              time.sleep(1)
 
-        if args.fasta_pep:
-            uuid_fa = wa.io.write_downloadable(org['commonName'], 'FASTA', sequences=seqs, seq_type='peptide')
-            if 'error' in uuid_fa or 'uuid' not in uuid_fa:
-                raise Exception("Apollo failed to prepare the file for download: %s" % uuid_fa)
-            args.fasta_pep.write(wa.io.download(uuid_fa['uuid'], output_format="text"))
+          if args.fasta_pep:
+              uuid_fa = wa.io.write_downloadable(org['commonName'], 'FASTA', sequences=seqs, seq_type='peptide')
+              if 'error' in uuid_fa or 'uuid' not in uuid_fa:
+                  raise Exception("Apollo failed to prepare the file for download: %s" % uuid_fa)
+              args.fasta_pep.write(wa.io.download(uuid_fa['uuid'], output_format="text"))
 
-        org_data.append(org)
+          org_data.append(org)
 
     args.json.write(json.dumps(org_data, indent=2))
