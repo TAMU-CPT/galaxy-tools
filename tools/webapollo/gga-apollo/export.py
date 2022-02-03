b87d8abf607653adf9629f8a1776dcc2cfd87bbc
crr <curtisross@tamu.edu>
2022-02-03 16:17:30 -0600

    add 'check_file...' function to each option, #561

--- tools/webapollo/gga-apollo/export.py
+++ tools/webapollo/gga-apollo/export.py
@@ -12,6 +12,35 @@ from arrow.apollo import get_apollo_instance
 
 from webapollo import UserObj, handle_credentials
 
+
+def check_file_can_be_written(obj, seq_type, export_type):
+    """
+    Checks if a file can be written based on if the returned object from Apollo is a null object or not.
+
+    If the object is null, it will exit the conditional block with an empty file.
+
+    This is to allow the delete organism tool to still process. TODO: We should modify it to write out in the file *why* it is empty.
+
+    If another error occurs, it will raise the exception and error out as before.
+
+    :type obj: IOClient object(?) TODO: Confirm
+    :param obj: WebApollo IOClient object.
+
+    :type seq_type: str
+    :param seq_type: Export selection. Choices: peptide, cds, cdna, genomic, or empty (ie '')
+
+    :type export_type: str
+    :param export_type: Export type. Choices: FASTA, GFF3, VCF
+    """
+
+    if "null object" in obj["error"]:
+        pass
+    else:
+        raise Exception(
+            f"Apollo failed to prepare the {seq_type} {export_type} file for download: {obj}"
+        )
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Script to export data from Apollo via web services"
@@ -68,61 +97,49 @@ if __name__ == "__main__":
                 org["commonName"], "GFF3", export_gff3_fasta=True, sequences=seqs
             )
             if "error" in uuid_gff or "uuid" not in uuid_gff:
-                raise Exception(
-                    "Apollo failed to prepare the GFF3 file for download: %s" % uuid_gff
-                )
-            args.gff.write(wa.io.download(uuid_gff["uuid"], output_format="text"))
-            time.sleep(1)
+                check_file_can_be_written(uuid_gff, "", "GFF3")
+            else:
+                args.gff.write(wa.io.download(uuid_gff["uuid"], output_format="text"))
+                time.sleep(1)
 
         if args.seq:
             uuid_fa = wa.io.write_downloadable(
                 org["commonName"], "FASTA", sequences=seqs, seq_type="genomic"
             )
             if "error" in uuid_fa or "uuid" not in uuid_fa:
-                raise Exception(
-                    "Apollo failed to prepare the FASTA file for download: %s" % uuid_fa
-                )
-            args.seq.write(wa.io.download(uuid_fa["uuid"], output_format="text"))
-            time.sleep(1)
+                check_file_can_be_written(uuid_fa, "genomic", "FASTA")
+            else:
+                args.seq.write(wa.io.download(uuid_fa["uuid"], output_format="text"))
+                time.sleep(1)
 
         if args.vcf:
             uuid_vcf = wa.io.write_downloadable(
                 org["commonName"], "VCF", sequences=seqs
             )
             if "error" in uuid_vcf or "uuid" not in uuid_vcf:
-                raise Exception(
-                    "Apollo failed to prepare the VCF file for download: %s" % uuid_vcf
-                )
-            args.vcf.write(wa.io.download(uuid_vcf["uuid"], output_format="text"))
-            time.sleep(1)
+                check_file_can_be_written(uuid_vcf, "", "VCF")
+            else:
+                args.vcf.write(wa.io.download(uuid_vcf["uuid"], output_format="text"))
+                time.sleep(1)
 
         if args.fasta_cdna:
             uuid_fa = wa.io.write_downloadable(
                 org["commonName"], "FASTA", sequences=seqs, seq_type="cdna"
             )
             if "error" in uuid_fa or "uuid" not in uuid_fa:
-                raise Exception(
-                    "Apollo failed to prepare the cdna FASTA file for download: %s"
-                    % uuid_fa
+                check_file_can_be_written(uuid_fa, "cdna", "FASTA")
+            else:
+                args.fasta_cdna.write(
+                    wa.io.download(uuid_fa["uuid"], output_format="text")
                 )
-            args.fasta_cdna.write(wa.io.download(uuid_fa["uuid"], output_format="text"))
-            time.sleep(1)
+                time.sleep(1)
 
         if args.fasta_cds:
             uuid_fa = wa.io.write_downloadable(
                 org["commonName"], "FASTA", sequences=seqs, seq_type="cds"
             )
-            # condition extended to catch a problem where the cds fasta could not be written for
-            # certain organisms. This should allow proper bypass in the event that this error
-            # occurs.
             if "error" in uuid_fa or "uuid" not in uuid_fa:
-                if "null object" in uuid_fa["error"]:
-                    pass
-                else:
-                    raise Exception(
-                        "Apollo failed to prepare the cds FASTA file for download: %s"
-                        % uuid_fa
-                    )
+                check_file_can_be_written(uuid_fa, "cds", "FASTA")
             else:
                 args.fasta_cds.write(
                     wa.io.download(uuid_fa["uuid"], output_format="text")
@@ -134,10 +151,11 @@ if __name__ == "__main__":
                 org["commonName"], "FASTA", sequences=seqs, seq_type="peptide"
             )
             if "error" in uuid_fa or "uuid" not in uuid_fa:
-                raise Exception(
-                    "Apollo failed to prepare the file for download: %s" % uuid_fa
+                check_file_can_be_written(uuid_fa, "peptide", "FASTA")
+            else:
+                args.fasta_pep.write(
+                    wa.io.download(uuid_fa["uuid"], output_format="text")
                 )
-            args.fasta_pep.write(wa.io.download(uuid_fa["uuid"], output_format="text"))
 
         org_data.append(org)
 
