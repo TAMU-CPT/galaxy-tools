6f032e4edcc9565cd46afc0f4b3b6db4c7eccb14
Anthony C <anthonyc@tamu.edu>
2022-02-07 10:14:45 -0600

    Suppress stderr issues

---
+++ tools/external/signalp6.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+import subprocess
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Python shell to suppress pipe errors in SignalP6")
+
+    parser.add_argument("--organism", type=str)
+    parser.add_argument("--inFile", type=str)
+
+    args = parser.parse_args()
+    cmd = "signalp6 -org " + args.organism + " -od ./subDir --format png --mode slow-sequential -ff " + args.inFile
+    try :
+        subprocess.run(cmd, shell=True) # Just to keep any sub-module errors Prophet throws to keep Galaxy from thinking the job failed
+    except :                            # The except is irrelevant, just needs to complete
+        res = 0
+    
+    
+    
+
